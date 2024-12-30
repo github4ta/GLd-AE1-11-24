65fdeac6ab8d9d949e6163c4c933cf6549b3fc4c
GutenMorgen <waldstall@gmail.com>
2024-12-30 21:54:27 +0300

    add Vladimir Bordiyan  int Multiply

--- src/Calculator.java
+++ src/Calculator.java
@@ -24,4 +24,8 @@ public class Calculator {
     public static double add(double a, double b) {
         return a + b;
     }
+
+    public static int add(int a, int b) {
+        return a * b;
+    }
 }
