282441e1ab9d6b20656ef9fdcc768a9d70dbac85
Nikolai <n91056379@gmail.com>
2024-12-30 21:54:02 +0300

    add method byte multiply

--- src/Calculator.java
+++ src/Calculator.java
@@ -24,4 +24,7 @@ public class Calculator {
     public static double add(double a, double b) {
         return a + b;
     }
+    public static double multiply(double a, double b){
+        return a * b;
+    }
 }
