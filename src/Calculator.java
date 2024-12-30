b860abbfc2d732ad39160fd013e0eedead597f2d
Nikolai <n91056379@gmail.com>
2024-12-30 21:38:19 +0300

    add method byte sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,7 +11,10 @@ public class Calculator {
     // Alexander Linnik
 
     // Nikolai Psekha
-
+     public static byte sum(byte a,byte b){
+         byte result = (byte) (a + b);
+         return result;
+     }
     // Natallia Taranko
 
 }
