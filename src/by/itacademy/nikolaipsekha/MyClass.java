187553de90bfa503818c6bb41d7e956dffb24688
Nikolai <n91056379@gmail.com>
2025-01-08 23:22:01 +0300

    add method in which all numbers are greater then zero

--- src/by/itacademy/nikolaipsekha/MyClass.java
+++ src/by/itacademy/nikolaipsekha/MyClass.java
@@ -1,5 +1,9 @@
 package by.itacademy.nikolaipsekha;
 
 public class MyClass {
-
+public static String numbersOfChange( int a, int b, int c){
+    if (a > 0 & b > 0 & c > 0){
+        return " exellent ";
+    }
+}
 }
