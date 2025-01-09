f8879b5549bed7d64ef83c0f899f95ed7f975ce1
Nikolai <n91056379@gmail.com>
2025-01-09 08:49:58 +0300

    add condition "bad"

--- src/by/itacademy/nikolaipsekha/MyClass.java
+++ src/by/itacademy/nikolaipsekha/MyClass.java
@@ -1,13 +1,16 @@
 package by.itacademy.nikolaipsekha;
 
 public class MyClass {
-public static String numbersOfChange( int a, int b, int c){
-    if (a > 0 & b > 0 & c > 0){
-        return " exellent ";
-    } else if (a > 0 && b > 0 && c < 0) {
-        return " good ";
-    } else if (a > 0 && b < 0 && c < 0) {
-return " fine ";
+    public static String numbersOfChange(int a, int b, int c) {
+        if (a > 0 & b > 0 & c > 0) {
+            return " exellent ";
+        } else if (a > 0 && b > 0 && c < 0) {
+            return " good ";
+        } else if (a > 0 && b < 0 && c < 0) {
+            return " fine ";
+        } else if (a < 0 && b < 0 && c < 0) {
+            return " bad ";
+        }
+        return " ";
     }
-}
 }
