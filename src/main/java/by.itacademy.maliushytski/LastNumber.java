ef161fbb6ecb74c0c115fa8465ca37430e912952
a.maliushytski <a.maliushytski@gmail.com>
2025-01-11 21:25:43 +0100

    ADD: added handling of irrelevant numbers in sendLastNumber method and few more tests

--- src/main/java/by.itacademy.maliushytski/LastNumber.java
+++ src/main/java/by.itacademy.maliushytski/LastNumber.java
@@ -1,6 +1,9 @@
 public class LastNumber {
     public static String sendLastNumber(int number) {
-        int lastNumber = number % 10;
-        return String.format("%s - последняя цифра числа %s", lastNumber, number);
+        if (number >= 10 || number <= -10) {
+            int lastNumber = number % 10;
+            return String.format("%s - последняя цифра числа %s", lastNumber, number);
+        }
+        return "irrelevant number";
     }
 }
