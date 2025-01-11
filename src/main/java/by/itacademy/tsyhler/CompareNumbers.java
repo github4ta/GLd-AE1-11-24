4a96165585d49ed43aa12463ce93609a52e79cbb
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-01-11 18:06:00 +0300

    refactor method compareWithZero

--- src/main/java/by/itacademy/tsyhler/CompareNumbers.java
+++ src/main/java/by/itacademy/tsyhler/CompareNumbers.java
@@ -1,24 +1,14 @@
 package by.itacademy.tsyhler;
 
 import java.util.Arrays;
-//Дано
-//Три целых числа.
-//        Написать код, который будет выводить в консоль сообщение:
-//        - “excellent” если все три числа больше 0,
-//        - “good” если два числа больше 0,
-//        - “fine” если одно число больше 0,
-//        - “bad” если нет ни одного числа больше 0.
-//Оформить решение в виде метода в классе (названия на ваше усмотрение)
-//Написать тесты на проверки метода.
-//Не забываем комитить АТОМАРНЫЕ шаги.
-//Соощаем о вашем решении здесь (Replay in thread) ссылкой на ваш PR.
 
 public class CompareNumbers {
     public static String compareWithZero(int a, int b, int c) {
         int[] numbers = {a, b, c};
-        if (Arrays.stream(numbers).filter(s -> s > 0).count() == 3) return "excellent";
-        if (Arrays.stream(numbers).filter(s -> s > 0).count() == 2) return "good";
-        if (Arrays.stream(numbers).filter(s -> s > 0).count() == 1) return "fine";
+        int positiveCount = (int) Arrays.stream(numbers).filter(s -> s > 0).count();
+        if (positiveCount == 3) return "excellent";
+        if (positiveCount == 2) return "good";
+        if (positiveCount == 1) return "fine";
         return "bad";
     }
 
