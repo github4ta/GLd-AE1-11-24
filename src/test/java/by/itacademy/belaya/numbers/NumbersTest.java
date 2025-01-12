30a4e68d4dc65590998a6f83a9d0fba071aa608b
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 02:14:10 +0300

    add test All numbers are positive to class NumbersTest

--- src/test/java/by/itacademy/belaya/numbers/NumbersTest.java
+++ src/test/java/by/itacademy/belaya/numbers/NumbersTest.java
@@ -1,6 +1,17 @@
 package by.itacademy.belaya.numbers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class NumbersTest {
+
+    @Test
+    @DisplayName("All numbers are positive")
+    public void test1() {
+        Assertions.assertEquals("excellent",
+                Numbers.classify(1, Integer.MAX_VALUE, 20));
+    }
 
 }
 
