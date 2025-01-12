ea030e82e340914ceec98cfd430cf09a25e1e9f8
GutenMorgen <waldstall@gmail.com>
2025-01-13 02:49:34 +0300

    add solution

--- src/test/java/it.academy.bordiyan/MultiDigitNumberTest.java
+++ src/test/java/it.academy.bordiyan/MultiDigitNumberTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 public class MultiDigitNumberTest {
 
     @Test
-    public void digitNumberTest1(){
-        Assertions.assertEquals();
+    public void digitNumberTest1() {
+        Assertions.assertEquals("3 - последняя цифра числа 23", MultiDigitNumber.digitNumber(23));
     }
 
 }
