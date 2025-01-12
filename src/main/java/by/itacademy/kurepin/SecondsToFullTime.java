4c8c24ddb99d67ffb43009a392aba5e0fa73cdaa
Артём <artem.kurepin999@gmail.com>
2025-01-12 21:46:00 +0300

    format information display

--- src/main/java/by/itacademy/kurepin/SecondsToFullTime.java
+++ src/main/java/by/itacademy/kurepin/SecondsToFullTime.java
@@ -2,7 +2,16 @@ package by.itacademy.kurepin;
 
 public class SecondsToFullTime {
     public static String secondsConvert(int sec) {
-        String time = String.format("%d часов %d минут %d секунд", sec / 3600, (sec % 3600) / 60, sec % 60);
+        int hours=sec/3600;
+        int minutes=(sec%3600)/60;
+        int seconds=sec%60;
+        String time;
+        if (hours==0){
+             time = String.format("%d минут %d секунд", minutes, seconds);
+        }else {
+            time = String.format("%d часов %d минут %d секунд", hours, minutes, seconds);
+        }
+
         return time;
     }
 
