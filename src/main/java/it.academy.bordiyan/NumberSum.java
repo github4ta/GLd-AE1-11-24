8658c5eeab33435438a877cb1019f22719d6ff86
GutenMorgen <waldstall@gmail.com>
2025-01-12 22:15:21 +0300

    add solution

--- src/main/java/it.academy.bordiyan/NumberSum.java
+++ src/main/java/it.academy.bordiyan/NumberSum.java
@@ -1,21 +1,21 @@
 package it.academy.bordiyan;
 
 public class NumberSum {
-    public static int numberSum() {
-        int number = 2314;
+    public static int numberSumParts(int number) {
+        number = 2314;
         String numStr = String.valueOf(number);
 
         int ab = Integer.parseInt(numStr.substring(0, 2));
         int cd = Integer.parseInt(numStr.substring(2));
 
-        return ab + cd;
+        return  ab + cd;
     }
 }
 
 class Main {
     public static void main(String[] args) {
         NumberSum numberSum = new NumberSum();
-        System.out.println(NumberSum.numberSum());
+        System.out.println(NumberSum.numberSumParts(2314));
     }
 }
 
--- src/test/java/it.academy.bordiyan/NumberSum.java
+++ src/test/java/it.academy.bordiyan/NumberSum.java
@@ -1,5 +1,9 @@
 package it.academy.bordiyan;
 
+import org.junit.jupiter.api.Test;
+
+
+
 
 
 
