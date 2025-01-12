a6c155118ed61c2a4e6644c5b674596bf96aeac4
GutenMorgen <waldstall@gmail.com>
2025-01-13 02:39:41 +0300

    add solution

--- src/main/java/it.academy.bordiyan/MultiDigitNumber.java
+++ src/main/java/it.academy.bordiyan/MultiDigitNumber.java
@@ -1,7 +1,12 @@
 package it.academy.bordiyan;
 
 public class MultiDigitNumber {
-    public static String digitNumber(int number){
+    public static String digitNumber(int number) {
+        if (number > 10) {
+            return "Число должно быть многозначным, содержать 2 и более цифры";
+        }
 
+        int digitNumber = number % 10;
+        return
     }
 }
