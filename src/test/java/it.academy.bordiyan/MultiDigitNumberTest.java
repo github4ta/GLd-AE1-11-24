5602c1ab4e0437df613942b77780cb77bd190877
GutenMorgen <waldstall@gmail.com>
2025-01-13 02:46:41 +0300

    add solution

---
+++ src/test/java/it.academy.bordiyan/MultiDigitNumberTest.java
@@ -0,0 +1,13 @@
+package it.academy.bordiyan;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MultiDigitNumberTest {
+
+    @Test
+    public void digitNumberTest1(){
+        Assertions.assertEquals();
+    }
+
+}
