ee10b05240f53cdc6c1344d9911e7a6ac7d1f5b7
GutenMorgen <waldstall@gmail.com>
2025-01-12 21:31:36 +0300

    add solution

--- src/main/java/it.academy.bordiyan/NumberSum.java
+++ src/main/java/it.academy.bordiyan/NumberSum.java
@@ -4,5 +4,8 @@ public class NumberSum {
     public static int numberSum(){
         int number = 2314;
         String numStr = String.valueOf(number);
+
+        int ab = Integer.parseInt(numStr.substring(0, 2));
+        System.out.println(ab);
     }
 }
