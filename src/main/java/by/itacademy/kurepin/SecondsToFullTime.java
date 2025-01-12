ecfc87a2808777072fd06bd9b7dcd371d63dc5ff
Артём <artem.kurepin999@gmail.com>
2025-01-12 21:35:50 +0300

    add method secondsConvert with solution

--- src/main/java/by/itacademy/kurepin/SecondsToFullTime.java
+++ src/main/java/by/itacademy/kurepin/SecondsToFullTime.java
@@ -1,6 +1,9 @@
 package by.itacademy.kurepin;
 
 public class SecondsToFullTime {
-
+    public static String secondsConvert(int sec) {
+        String time = String.format("%d часов %d минут %d секунд", sec / 3600, (sec % 3600) / 60, sec % 60);
+        return time;
+    }
 
 }
