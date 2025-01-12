1d42c30f3d509dbfcc4d6cab4988c1633154dee1
GutenMorgen <waldstall@gmail.com>
2025-01-13 02:54:57 +0300

    add solution

--- src/test/java/it.academy.bordiyan/MultiDigitNumberTest.java
+++ src/test/java/it.academy.bordiyan/MultiDigitNumberTest.java
@@ -10,4 +10,13 @@ public class MultiDigitNumberTest {
         Assertions.assertEquals("3 - последняя цифра числа 23", MultiDigitNumber.digitNumber(23));
     }
 
+    @Test
+    public void digitNumberTest2() {
+        Assertions.assertEquals("4 - последняя цифра числа 124", MultiDigitNumber.digitNumber(124));
+    }
+
+    @Test
+    public void digitNumberTest3() {
+        Assertions.assertEquals("8 - последняя цифра числа 2345678", MultiDigitNumber.digitNumber(2345678));
+    }
 }
