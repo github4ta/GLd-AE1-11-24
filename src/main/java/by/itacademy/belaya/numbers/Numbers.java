1b5dfb24746e99132885e6892ae5e67301388c04
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 02:05:06 +0300

    add method classify to class Numbers

--- src/main/java/by/itacademy/belaya/numbers/Numbers.java
+++ src/main/java/by/itacademy/belaya/numbers/Numbers.java
@@ -1,5 +1,26 @@
 package by.itacademy.belaya.numbers;
 
 public class Numbers {
-
+    public static String classify(int number1, int number2, int number3) {
+        int count = 0;
+        if (number1 > 0) {
+            count++;
+        }
+        if (number2 > 0) {
+            count++;
+        }
+        if (number3 > 0) {
+            count++;
+        }
+        switch (count) {
+            case 3:
+                return "excellent";
+            case 2:
+                return "good";
+            case 1:
+                return "fine";
+            default:
+                return "bad";
+        }
+    }
 }
