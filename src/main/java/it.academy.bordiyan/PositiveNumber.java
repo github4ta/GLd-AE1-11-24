8f83edea9a559176cb23a1802ac0946fa1f73fe5
GutenMorgen <waldstall@gmail.com>
2025-01-12 19:34:56 +0300

    add solution

--- src/main/java/it.academy.bordiyan/PositiveNumber.java
+++ src/main/java/it.academy.bordiyan/PositiveNumber.java
@@ -1,4 +1,20 @@
 package it.academy.bordiyan;
 
 public class PositiveNumber {
+    public static String positiveNumber(int a, int b, int c) {
+        if (a > 0 & b > 0 & c > 0) {
+            return "excellent - все три числа больше 0";
+        }
+        if ((a > 0 & b > 0 & c < 0) || (a > 0 & b < 0 & c > 0) || (a < 0 & b > 0 & c > 0)) {
+            return "good - два числа больше 0";
+        }
+        if ((a > 0 & b < 0 & c < 0) || (a < 0 & b > 0 & c < 0) || (a < 0 & b < 0 & c > 0)) {
+            return "fine - одно числа больше 0";
+        }
+        if (a < 0 & b < 0 & c < 0) {
+            return "Bad - ни одного числа больше 0";
+        } else {
+            return "Вы вели все нули";
+        }
+    }
 }
