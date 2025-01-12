3f75448fedc661c937336b7070367c5d2afc2e8a
Nikolai <n91056379@gmail.com>
2025-01-12 19:45:09 +0300

    refactor method

--- src/main/java/by/itacademy/psekha/ChangeOfNumber.java
+++ src/main/java/by/itacademy/psekha/ChangeOfNumber.java
@@ -2,15 +2,13 @@ package by.itacademy.psekha;
 
 public class ChangeOfNumber {
     public static String numbersOfChange(int a, int b, int c) {
-        //  if (a > 0 & b > 0 & c > 0) {
-        //  return String.format("a=%s, b=%s, c=%s excellent", a, b, c);
         if (a > 0 && b > 0 && c < 0 || a > 0 && b < 0 && c > 0 || a < 0 && b > 0 && c > 0) {
             return "good";
         } else if (a > 0 && b < 0 && c < 0 || a < 0 && b > 0 && c < 0 || a < 0 && b < 0 && c > 0) {
             return "fine";
         } else if (a < 0 & b < 0 & c < 0) {
             return "bad";
-        } else if (a==0 & b==0 & c==0) {
+        } else if (a == 0 & b == 0 & c == 0) {
             return "not found numbers";
         }
         return "excellent";
