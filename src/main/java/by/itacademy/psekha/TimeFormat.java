0e15d86b56700ffd5a44fd5b84314ba339c72ed1
Nikolai <n91056379@gmail.com>
2025-01-12 17:50:13 +0300

    refactor method

--- src/main/java/by/itacademy/psekha/TimeFormat.java
+++ src/main/java/by/itacademy/psekha/TimeFormat.java
@@ -1,10 +1,10 @@
 public class TimeFormat {
     public static String getTime(int seconds) {
         int sec = 1249;
-        String time = String.format(" %d минут %d секунд", (sec % 3600) / 60, sec % 60);
+        String time = String.format("%d минут %d секунд", (sec % 3600) / 60, sec % 60);
         return time;
     }
-    public static String getTime(String[] args) {
+    public static String getTime2(int seconds) {
         int sec = 648958;
         String time = String.format("%03d часов %02d минут %02d секунд", sec / 3600, (sec % 3600) / 60, sec % 60);
         return time;
