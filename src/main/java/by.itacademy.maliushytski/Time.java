b9d12d3ae22b285e475de49c06470b5874dac847
a.maliushytski <a.maliushytski@gmail.com>
2025-01-12 21:16:10 +0100

    MOD: made changes according to the comments left

--- src/main/java/by.itacademy.maliushytski/Time.java
+++ src/main/java/by.itacademy.maliushytski/Time.java
@@ -1,11 +1,13 @@
 public class Time {
     public static String setTimeFormatFromSeconds(int seconds) {
-        if (seconds <= 0) {
+        if (isValid(seconds)) {
             throw new IllegalArgumentException("No seconds to convert");
         }
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int lastSeconds = seconds % 60;
+
+        int hours = getHours(seconds);
+        int minutes = getMinutes(seconds);
+        int lastSeconds = getSeconds(seconds);
+
         if (hours > 0) {
             return String.format("%s часов %s минут %s секунд", hours, minutes, lastSeconds);
         } else if (minutes > 0) {
@@ -13,5 +15,21 @@ public class Time {
         } else {
             return String.format("%s секунд", lastSeconds);
         }
+    }
+
+    private static boolean isValid(int seconds) {
+        return seconds <= 0;
+    }
+
+    private static int getHours(int seconds) {
+        return seconds / 3600;
+    }
+
+    private static int getMinutes(int seconds) {
+        return (seconds % 3600) / 60;
+    }
+
+    private static int getSeconds(int seconds) {
+        return seconds % 60;
     }
 }
