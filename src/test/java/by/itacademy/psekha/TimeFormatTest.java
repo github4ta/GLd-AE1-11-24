ffa719cad4e42240d6e39eea726a4fe66ad4f6cf
Nikolai <n91056379@gmail.com>
2025-01-12 17:51:24 +0300

    add  testTime

---
+++ src/test/java/by/itacademy/psekha/TimeFormatTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TimeFormatTest {
+    @Test
+    public void testTime() {
+        Assertions.assertEquals("20 минут 49 секунд", TimeFormat.getTime(1249));
+    }
+
+    @Test
+    public void testTime1() {
+        Assertions.assertEquals("180 часов 15 минут 58 секунд", TimeFormat.getTime2(648958));
+    }
+}
