a981661b8927fb8e4a4645dc9334ce359828ca3e
Артём <artem.kurepin999@gmail.com>
2025-01-12 21:23:07 +0300

    add method summOfHalfs

--- src/main/java/by/itacademy/kurepin/FourDigitNums.java
+++ src/main/java/by/itacademy/kurepin/FourDigitNums.java
@@ -1,4 +1,13 @@
 package by.itacademy.kurepin;
 
 public class FourDigitNums {
+    public static int summOfHalfs(int number){
+        int firstHalf=Integer.parseInt((""+number/1000)+(""+number%1000/100));
+        int secondHalf=Integer.parseInt((""+number%100/10)+(""+number%10));
+
+        return firstHalf+secondHalf;
+    }
+
+
+
 }
