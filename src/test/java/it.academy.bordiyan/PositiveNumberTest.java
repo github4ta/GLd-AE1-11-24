ae4ad81ddfd0f4d06398a7c72294941ee486578b
GutenMorgen <waldstall@gmail.com>
2025-01-12 20:15:31 +0300

    add solution

--- src/test/java/it.academy.bordiyan/PositiveNumberTest.java
+++ src/test/java/it.academy.bordiyan/PositiveNumberTest.java
@@ -16,16 +16,35 @@ public class PositiveNumberTest {
 
     @Test
     public void testPositiveNumber3() {
-        Assertions.assertEquals("fine - одно числа больше 0", PositiveNumber.positiveNumber(-5, 5, -5));
+        Assertions.assertEquals("good - два числа больше 0", PositiveNumber.positiveNumber(5, -5, 5));
     }
-
     @Test
     public void testPositiveNumber4() {
-        Assertions.assertEquals("Bad - ни одного числа больше 0", PositiveNumber.positiveNumber(-5, -5, -5));
+        Assertions.assertEquals("good - два числа больше 0", PositiveNumber.positiveNumber(5, 5, -5));
     }
 
     @Test
     public void testPositiveNumber5() {
+        Assertions.assertEquals("fine - одно числа больше 0", PositiveNumber.positiveNumber(5, -5, -5));
+    }
+
+    @Test
+    public void testPositiveNumber6() {
+        Assertions.assertEquals("fine - одно числа больше 0", PositiveNumber.positiveNumber(-5, 5, -5));
+    }
+
+    @Test
+    public void testPositiveNumber7() {
+        Assertions.assertEquals("fine - одно числа больше 0", PositiveNumber.positiveNumber(-5, -5, 5));
+    }
+
+    @Test
+    public void testPositiveNumber8() {
+        Assertions.assertEquals("Bad - ни одного числа больше 0", PositiveNumber.positiveNumber(-5, -5, -5));
+    }
+
+    @Test
+    public void testPositiveNumber9() {
         Assertions.assertEquals("Вы ввели все нули", PositiveNumber.positiveNumber(0, 0, 0));
     }
 }
