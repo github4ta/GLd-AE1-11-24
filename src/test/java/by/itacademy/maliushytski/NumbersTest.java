96a90d40ec5711f1bf1b45932bb62fdf8ae250c1
a.maliushytski <a.maliushytski@gmail.com>
2025-01-12 21:16:10 +0100

    MOD: made changes according to the comments left

--- src/test/java/by/itacademy/maliushytski/NumbersTest.java
+++ src/test/java/by/itacademy/maliushytski/NumbersTest.java
@@ -5,37 +5,37 @@ import org.junit.jupiter.api.Test;
 public class NumbersTest {
     @Test
     @Description("Test defineZeros method with all zeros")
-    public void checkZeros() {
+    public void testZeros() {
         Assertions.assertEquals("bad", Numbers.defineZeros(0, 0, 0));
     }
 
     @Test
     @Description("Test defineZeros method with one zero")
-    public void checkOneZero() {
+    public void testOneZero() {
         Assertions.assertEquals("good", Numbers.defineZeros(2, 1, 0));
     }
 
     @Test
     @Description("Test defineZeros method with two zeros")
-    public void checkTwoZeros() {
+    public void testTwoZeros() {
         Assertions.assertEquals("fine", Numbers.defineZeros(0, 0, 120));
     }
 
     @Test
     @Description("Test defineZeros method without zeros")
-    public void checkWithoutZeros() {
+    public void testWithoutZeros() {
         Assertions.assertEquals("excellent", Numbers.defineZeros(1, 2, 3));
     }
 
     @Test
     @Description("Test defineZeros method with all zeros")
-    public void checkNegativeValues() {
+    public void testNegativeValues() {
         Assertions.assertEquals("bad", Numbers.defineZeros(0, -10, -3));
     }
 
     @Test
     @Description("Test defineZeros method with large max values")
-    public void checkLargeMaxValues() {
+    public void testLargeMaxValues() {
         Assertions.assertEquals("excellent", Numbers.defineZeros(Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE));
     }
 
