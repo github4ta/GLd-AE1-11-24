b582e5e3b60103e2d0e31aa5e38de74284fab1a0
a.maliushytski <a.maliushytski@gmail.com>
2025-01-12 20:55:34 +0100

    MOD: made changes in findSumHalvesFourDigits class according to the comment left

--- src/main/java/by.itacademy.maliushytski/FourDigitNumber.java
+++ src/main/java/by.itacademy.maliushytski/FourDigitNumber.java
@@ -1,13 +1,20 @@
 public class FourDigitNumber {
     public static int findSumHalvesFourDigits(int number) {
-        String stringNumber = String.valueOf(number);
+        //проверяем можно ли дальше работать с числом
         if (number < 0) {
             throw new IllegalArgumentException("Число отрицательное");
-        } else if (number == 0) {
+        }
+        //проверяем равно ли введенное значение нулю
+        if (number == 0) {
             return 0;
-        } else if (stringNumber.length() != 4) {
+        }
+
+        String stringNumber = String.valueOf(number);
+        //прверяем действительно ли занчение состоит их 4 знаков
+        if (stringNumber.length() != 4) {
             throw new IllegalArgumentException("Число не четырехзначное");
         }
+
         int firstPart = Integer.parseInt(stringNumber.substring(0, 2));
         int secondPart = Integer.parseInt(stringNumber.substring(2));
 
