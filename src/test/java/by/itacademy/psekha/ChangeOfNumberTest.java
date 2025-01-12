649da6751c0e793cda332c65356c38f2c558a202
Nikolai <n91056379@gmail.com>
2025-01-12 17:00:07 +0300

    add test

--- src/test/java/by/itacademy/psekha/ChangeOfNumberTest.java
+++ src/test/java/by/itacademy/psekha/ChangeOfNumberTest.java
@@ -4,8 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ChangeOfNumberTest {
+ //   public static String expectedOfChange( String word){
+          //      return  word;
+   // }
     @Test
     public  void test(){
-        Assertions.assertEquals(0, by.itacademy.psekha.ChangeOfNumber.numbersOfChange(2,3,4));
+        Assertions.assertEquals( "not found numbers" , by.itacademy.psekha.ChangeOfNumber.numbersOfChange(0,0,0));
+    }
+    @Test
+    public  void test1(){
+        Assertions.assertEquals( "bad" , by.itacademy.psekha.ChangeOfNumber.numbersOfChange(-7,-11,-1));
+    }
+    @Test
+    public  void test2(){
+        Assertions.assertEquals( "fine" , by.itacademy.psekha.ChangeOfNumber.numbersOfChange(-2,-4,9));
+    }
+    @Test
+    public  void test3(){
+        Assertions.assertEquals( "good" , by.itacademy.psekha.ChangeOfNumber.numbersOfChange(20,7,-4));
+    }
+    @Test
+    public  void test4(){
+        Assertions.assertEquals( "excellent" , by.itacademy.psekha.ChangeOfNumber.numbersOfChange(10,20,30));
     }
 }
