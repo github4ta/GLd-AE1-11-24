570e0919408243667c5acde774b187eeb8c9ba22
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 18:49:45 +0300

    add test Number is not four-digit

--- src/test/java/by/itacademy/belaya/digitsum/DigitSumTest.java
+++ src/test/java/by/itacademy/belaya/digitsum/DigitSumTest.java
@@ -1,5 +1,17 @@
 package by.itacademy.belaya.digitsum;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DigitSumTest {
 
+    @Test
+    @DisplayName("Number is not four-digit")
+    public void test1() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> DigitSum.getSumOfTwoPartsOfNumber(103));
+        Assertions.assertEquals("Число должно быть четырехзначным", exception.getMessage());
+    }
 }
