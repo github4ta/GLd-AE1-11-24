99b9b18a59c8dd98156302370bd8e58815d9550a
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 14:24:58 +0300

    add method isPositive, refactor conditions, add blank lines

--- src/main/java/by/itacademy/belaya/numbers/Numbers.java
+++ src/main/java/by/itacademy/belaya/numbers/Numbers.java
@@ -1,17 +1,20 @@
 package by.itacademy.belaya.numbers;
 
 public class Numbers {
+
     public static String getClassificationForNumbers(int number1, int number2, int number3) {
         int count = 0;
-        if (number1 > 0) {
+
+        if (isPositive(number1)) {
             count++;
         }
-        if (number2 > 0) {
+        if (isPositive(number2)) {
             count++;
         }
-        if (number3 > 0) {
+        if (isPositive(number3)) {
             count++;
         }
+
         switch (count) {
             case 3:
                 return "excellent";
@@ -22,5 +25,9 @@ public class Numbers {
             default:
                 return "bad";
         }
+    }
+
+    private static boolean isPositive(int number) {
+        return number > 0;
     }
 }
