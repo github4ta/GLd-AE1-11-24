045c7e3f158cd2727d03060c4cb121895834510e
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 16:24:25 +0300

    add method isMultiDigitNumber

--- src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
+++ src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
@@ -2,4 +2,7 @@ package by.itacademy.belaya.multidigitnumber;
 
 public class MultiDigitNumber {
 
+    private static boolean isMultiDigitNumber(long number) {
+        return Math.abs(number) >= 10;
+    }
 }
