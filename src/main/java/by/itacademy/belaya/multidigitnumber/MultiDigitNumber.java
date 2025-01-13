4dbb7aca123dfad542116805830e9295587c8265
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 16:25:41 +0300

    add method getMessageAboutLastDigit

--- src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
+++ src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
@@ -2,6 +2,17 @@ package by.itacademy.belaya.multidigitnumber;
 
 public class MultiDigitNumber {
 
+    public static String getMessageAboutLastDigit(long number) {
+        if (!isMultiDigitNumber(number)) {
+            throw new IllegalArgumentException("Число должно быть многозначным");
+        }
+        return new StringBuilder()
+                .append(getLastDigit(number))
+                .append(" - последняя цифра числа ")
+                .append(number)
+                .toString();
+    }
+
     private static boolean isMultiDigitNumber(long number) {
         return Math.abs(number) >= 10;
     }
