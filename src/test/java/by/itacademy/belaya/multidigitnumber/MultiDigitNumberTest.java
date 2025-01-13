c05abfeed8280be786f2ac5e2e8d528e194412c8
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 16:28:03 +0300

    add test Positive multi-digit number

--- src/test/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumberTest.java
+++ src/test/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumberTest.java
@@ -1,5 +1,15 @@
 package by.itacademy.belaya.multidigitnumber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MultiDigitNumberTest {
 
+    @Test
+    @DisplayName("Positive multi-digit number")
+    public void test1() {
+        Assertions.assertEquals("5 - последняя цифра числа 15",
+                MultiDigitNumber.getMessageAboutLastDigit(15));
+    }
 }
