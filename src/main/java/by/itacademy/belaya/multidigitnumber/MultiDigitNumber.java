185da6796a18a125683bab3b5db25e631570e7d1
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 16:24:59 +0300

    add method getLastDigit

--- src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
+++ src/main/java/by/itacademy/belaya/multidigitnumber/MultiDigitNumber.java
@@ -5,4 +5,8 @@ public class MultiDigitNumber {
     private static boolean isMultiDigitNumber(long number) {
         return Math.abs(number) >= 10;
     }
+
+    private static long getLastDigit(long number) {
+        return Math.abs(number % 10);
+    }
 }
