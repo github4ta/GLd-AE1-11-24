1b938eb95e13e08f831d5b94d85115537719c25c
a.maliushytski <a.maliushytski@gmail.com>
2025-01-13 20:28:39 +0100

    MOD: made changes according to the comments left

--- src/main/java/by.itacademy.maliushytski/Calendar.java
+++ src/main/java/by.itacademy.maliushytski/Calendar.java
@@ -1,75 +1,53 @@
 public class Calendar {
-    public static final int JANUARY = 1;//31
-    public static final int FEBRUARY = 2;//28
-    public static final int MARCH = 3;//31
-    public static final int APRIL = 4;//30
-    public static final int MAY = 5;//31
-    public static final int JUNE = 6;//30
-    public static final int JULY = 7;//31
-    public static final int AUGUST = 8;//31
-    public static final int SEPTEMBER = 9;//30
-    public static final int OCTOBER = 10;//31
-    public static final int NOVEMBER = 11;//30
-    public static final int DECEMBER = 12;//31
-
     public static String getDayAndMonths(int day, int month) {
         if (day <= 0 || month <= 0) {
             throw new IllegalArgumentException("Incorrect value");
         }
 
+        Month enumMonth = Month.fromInt(month);
+
         if (month > 12 || month < 1) {
             return "This date does not exist";
         }
 
-        if ((month == JANUARY || month == MARCH || month == MAY || month == JULY || month == AUGUST || month == OCTOBER || month == DECEMBER) && day > 31) {
+        if ((enumMonth == Month.JANUARY || enumMonth == Month.MARCH || enumMonth == Month.MAY || enumMonth == Month.JULY
+                || enumMonth == Month.AUGUST || enumMonth == Month.OCTOBER || enumMonth == Month.DECEMBER) && day > 31) {
             return "This date does not exist";
         }
 
-        if ((month == APRIL || month == JUNE || month == SEPTEMBER || month == NOVEMBER) && day > 30) {
+        if ((enumMonth == Month.APRIL || enumMonth == Month.JUNE || enumMonth == Month.SEPTEMBER ||
+                enumMonth == Month.NOVEMBER) && day > 30) {
             return "This date does not exist";
         }
 
-        if (month == FEBRUARY && day > 28) {
+        if (enumMonth == Month.FEBRUARY && day > 28) {
             return "This date does not exist";
         }
 
-        if (month == JANUARY && (day == 1 || day == 2 || day == 7)) {
+        if (enumMonth == Month.JANUARY && (day == 1 || day == 2 || day == 7)) {
             return "Public holiday";
         }
-        if (month == MARCH && day == 8) {
+        if (enumMonth == Month.MARCH && day == 8) {
             return "Public holiday";
         }
-        if (month == MAY && (day == 1 || day == 9 || day == 14)) {
+        if (enumMonth == Month.MAY && (day == 1 || day == 9 || day == 14)) {
             return "Public holiday";
         }
-        if (month == JULY && day == 3) {
+        if (enumMonth == Month.JULY && day == 3) {
             return "Public holiday";
         }
-        if (month == NOVEMBER && day == 7) {
+        if (enumMonth == Month.NOVEMBER && day == 7) {
             return "Public holiday";
         }
-        if (month == DECEMBER && day == 25) {
+        if (enumMonth == Month.DECEMBER && day == 25) {
             return "Public holiday";
         }
-        return day + getDayPostfix(day) + " of " + getMonthName(month);
+        return day + getDayPostfix(day) + " of " + getFormattedMoth(enumMonth);
     }
 
-    private static String getMonthName(int month) {
-        return switch (month) {
-            case JANUARY -> "January";
-            case FEBRUARY -> "February";
-            case MARCH -> "March";
-            case APRIL -> "April";
-            case MAY -> "May";
-            case JUNE -> "June";
-            case JULY -> "July";
-            case AUGUST -> "August";
-            case SEPTEMBER -> "September";
-            case OCTOBER -> "October";
-            case NOVEMBER -> "November";
-            case DECEMBER -> "December";
-            default -> "Unknown";
-        };
+    private static String getFormattedMoth(Month enumMonth) {
+        String monthName = enumMonth.name();
+        return monthName.charAt(0) + monthName.substring(1).toLowerCase();
     }
 
     private static String getDayPostfix(int day) {
