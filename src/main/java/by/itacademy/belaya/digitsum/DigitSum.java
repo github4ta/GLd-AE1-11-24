566b38b3ec3683a20bbb1d54afe79442ab76d41e
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 18:39:55 +0300

    add method getFirstPartOfNumber

--- src/main/java/by/itacademy/belaya/digitsum/DigitSum.java
+++ src/main/java/by/itacademy/belaya/digitsum/DigitSum.java
@@ -5,4 +5,9 @@ public class DigitSum {
     private static boolean isFourDigitNumber(int number){
         return (number>=1000&&number<=9999)||(number<=-1000&&number>=-9999);
     }
+
+    private static int getFirstPartOfNumber(int number) {
+        return number / 100;
+    }
+
 }
