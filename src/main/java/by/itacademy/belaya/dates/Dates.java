e7ddfe71f313ab3d1bfc8af3c49644395db13a1d
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 14:48:04 +0300

    add method getClassificationForDates

--- src/main/java/by/itacademy/belaya/dates/Dates.java
+++ src/main/java/by/itacademy/belaya/dates/Dates.java
@@ -2,4 +2,12 @@ package by.itacademy.belaya.dates;
 
 public class Dates {
 
+    public static String getClassificationForDates(int month, int dayOfMonth) {
+        if (month < 1 || month > 12 || dayOfMonth < 1 || dayOfMonth > Month.getDaysInMonth(month)) {
+            return "такой даты не существует";
+        } else if (Holiday.isHoliday(month, dayOfMonth)) {
+            return "Праздничный день";
+        }
+        return dayOfMonth + " " + Month.getMonthNameInDate(month);
+    }
 }
