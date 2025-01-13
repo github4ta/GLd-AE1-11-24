94b5df4a2619780c597836131b5f6beb6a3ebc04
Nikolai <n91056379@gmail.com>
2025-01-13 16:07:25 +0300

    add condition holiday day

--- src/main/java/by/itacademy/psekha/CalendarData.java
+++ src/main/java/by/itacademy/psekha/CalendarData.java
@@ -11,7 +11,14 @@ public class CalendarData {
         if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
             return "there is no such date";
         }
-
+        if ((month == 1 && (day == 1 || day == 2 || day == 7)) ||
+                (month == 3 && day == 8) ||
+                (month == 5 && (day == 1 || day == 9 || day == 14)) ||
+                (month == 11 && day == 7) ||
+                (month == 12 && day == 25)) {
+            return "holiday day";
+        }
+        return Month.of(month) + " " + day;
     }
 }
 
