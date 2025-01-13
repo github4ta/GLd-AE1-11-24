b26a548209b6fb6f5fbb2a1d3fa16acc083449d9
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 14:38:42 +0300

    add Set HOLIDAYS

--- src/main/java/by/itacademy/belaya/dates/Holiday.java
+++ src/main/java/by/itacademy/belaya/dates/Holiday.java
@@ -1,5 +1,10 @@
 package by.itacademy.belaya.dates;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Holiday {
+
+    private static final Set<String> HOLIDAYS = new HashSet<>();
 
 }
