4e3c7d7fc1a6cdf66fe080eb458b4bf96df66bd3
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 16:33:43 +0300

    add test Invalid month and day

--- src/test/java/by/itacademy/belaya/dates/DatesTest.java
+++ src/test/java/by/itacademy/belaya/dates/DatesTest.java
@@ -1,5 +1,15 @@
 package by.itacademy.belaya.dates;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DatesTest {
 
+    @Test
+    @DisplayName("Invalid month and day")
+    public void test1() {
+        Assertions.assertEquals("такой даты не существует",
+                Dates.getClassificationForDates(0, -1));
+    }
 }
