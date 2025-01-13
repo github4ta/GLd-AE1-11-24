4ba578dac3705e337d0be88f140c67fb0056e260
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 18:39:23 +0300

    add method isFourDigitNumber

--- src/main/java/by/itacademy/belaya/digitsum/DigitSum.java
+++ src/main/java/by/itacademy/belaya/digitsum/DigitSum.java
@@ -2,4 +2,7 @@ package by.itacademy.belaya.digitsum;
 
 public class DigitSum {
 
+    private static boolean isFourDigitNumber(int number){
+        return (number>=1000&&number<=9999)||(number<=-1000&&number>=-9999);
+    }
 }
