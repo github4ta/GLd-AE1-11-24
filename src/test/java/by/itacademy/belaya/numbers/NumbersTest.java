a9c804948ec3da5ed600630c80d813eb3834e482
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-13 14:28:59 +0300

    add blank lines, change method name from Numbers.classify to Numbers.getClassificationForNumbers

--- src/test/java/by/itacademy/belaya/numbers/NumbersTest.java
+++ src/test/java/by/itacademy/belaya/numbers/NumbersTest.java
@@ -10,40 +10,42 @@ public class NumbersTest {
     @DisplayName("All numbers are positive")
     public void test1() {
         Assertions.assertEquals("excellent",
-                Numbers.classify(1, Integer.MAX_VALUE, 20));
+                Numbers.getClassificationForNumbers(1, Integer.MAX_VALUE, 20));
     }
+
     @Test
     @DisplayName("Two numbers are positive")
     public void test2() {
         Assertions.assertEquals("good",
-                Numbers.classify(0, Integer.MAX_VALUE, 20));
+                Numbers.getClassificationForNumbers(0, Integer.MAX_VALUE, 20));
     }
+
     @Test
     @DisplayName("One number is positive")
     public void test3() {
         Assertions.assertEquals("fine",
-                Numbers.classify(0, Integer.MAX_VALUE, -10));
+                Numbers.getClassificationForNumbers(0, Integer.MAX_VALUE, -10));
     }
 
     @Test
     @DisplayName("Numbers are not positive")
     public void test4() {
         Assertions.assertEquals("bad",
-                Numbers.classify(0, 0, -10));
+                Numbers.getClassificationForNumbers(0, 0, -10));
     }
 
     @Test
     @DisplayName("All numbers are zero")
     public void test5() {
         Assertions.assertEquals("bad",
-                Numbers.classify(0, 0, 0));
+                Numbers.getClassificationForNumbers(0, 0, 0));
     }
 
     @Test
     @DisplayName("All numbers are negative")
     public void test6() {
         Assertions.assertEquals("bad",
-                Numbers.classify(-1, -2, Integer.MIN_VALUE));
+                Numbers.getClassificationForNumbers(-1, -2, Integer.MIN_VALUE));
     }
 }
 
