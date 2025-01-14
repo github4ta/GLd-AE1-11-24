776700070a5986a59d3e76523fb2a3379626a77d
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-15 00:10:44 +0300

    add test Negative seconds

--- src/test/java/by/itacademy/belaya/time/TimeFormatterTest.java
+++ src/test/java/by/itacademy/belaya/time/TimeFormatterTest.java
@@ -1,4 +1,17 @@
 package by.itacademy.belaya.time;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TimeFormatterTest {
+
+    @Test
+    @DisplayName("Negative seconds")
+    public void test1() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> TimeFormatter.getFormattedTime(-100));
+        Assertions.assertEquals("Время не может быть отрицательным", exception.getMessage());
+    }
 }
