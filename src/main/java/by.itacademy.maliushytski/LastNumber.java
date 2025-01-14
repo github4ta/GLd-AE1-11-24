df06193098535897b14ce37a286d5e59b2a62bcb
a.maliushytski <a.maliushytski@gmail.com>
2025-01-14 19:10:02 +0100

    MOD: made changes according to the comments left

--- src/main/java/by.itacademy.maliushytski/LastNumber.java
+++ src/main/java/by.itacademy.maliushytski/LastNumber.java
@@ -1,9 +1,14 @@
 public class LastNumber {
-    public static String sendLastNumber(int number) {
-        if (number >= 10 || number <= -10) {
-            int lastNumber = number % 10;
-            return String.format("%s - последняя цифра числа %s", lastNumber, number);
+    public static String getLastDigitAsString(int number) {
+        int lowerBoundaryToStartProcess = -10;
+        int upperBoundaryToStartProcess = 10;
+        if (number >= upperBoundaryToStartProcess || number <= lowerBoundaryToStartProcess) {
+            return String.format("%s - последняя цифра числа %s", getLastDigit(number), number);
         }
         return "irrelevant number";
+    }
+
+    private static int getLastDigit(int number) {
+        return number % 10;
     }
 }
--- src/main/java/by.itacademy.maliushytski/Numbers.java
+++ src/main/java/by.itacademy.maliushytski/Numbers.java
@@ -4,10 +4,10 @@ public class Numbers {
     public static String defineZeros(int firstNumber, int secondNumber, int thirdNumber) {
         long qtyNonZeroNumbers = Stream.of(firstNumber, secondNumber, thirdNumber).filter(n -> n > 0).count();
         return switch ((int) qtyNonZeroNumbers) {
-            case 1 -> "fine";
-            case 2 -> "good";
-            case 3 -> "excellent";
-            default -> "bad";
+            case 1 -> Status.FINE.getDescription();
+            case 2 -> Status.GOOD.getDescription();
+            case 3 -> Status.EXCELLENT.getDescription();
+            default -> Status.BAD.getDescription();
         };
     }
 }
