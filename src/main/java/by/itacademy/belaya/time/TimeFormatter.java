773f1b6ad833cedb4c95e50a0d05b091449ae20a
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-15 00:08:58 +0300

    add method getFormattedTime

--- src/main/java/by/itacademy/belaya/time/TimeFormatter.java
+++ src/main/java/by/itacademy/belaya/time/TimeFormatter.java
@@ -4,6 +4,42 @@ import java.math.BigInteger;
 
 public class TimeFormatter {
 
+    public static String getFormattedTime(long totalSeconds) {
+
+        if (isNegative(totalSeconds)) {
+            throw new IllegalArgumentException("Время не может быть отрицательным");
+        }
+
+        BigInteger hours = getHours(totalSeconds);
+        BigInteger minutes = getMinutes(totalSeconds);
+        BigInteger remainingSeconds = getRemainingSeconds(totalSeconds);
+
+        StringBuilder formattedTime = new StringBuilder();
+
+        if (hours.compareTo(BigInteger.ZERO) > 0) {
+            formattedTime.append(hours)
+                    .append(" ")
+                    .append(TimeUnitFormatter.HOURS.getWordForm(hours.longValue()));
+        }
+        if (minutes.compareTo(BigInteger.ZERO) > 0) {
+            if (!formattedTime.isEmpty()) {
+                formattedTime.append(" ");
+            }
+            formattedTime.append(minutes)
+                    .append(" ")
+                    .append(TimeUnitFormatter.MINUTES.getWordForm(minutes.longValue()));
+        }
+        if (remainingSeconds.compareTo(BigInteger.ZERO) > 0 || formattedTime.isEmpty()) {
+            if (!formattedTime.isEmpty()) {
+                formattedTime.append(" ");
+            }
+            formattedTime.append(remainingSeconds)
+                    .append(" ")
+                    .append(TimeUnitFormatter.SECONDS.getWordForm(remainingSeconds.longValue()));
+        }
+        return formattedTime.toString();
+    }
+
     private static BigInteger getHours(long totalSeconds) {
         return BigInteger.valueOf(totalSeconds / 3600);
     }
