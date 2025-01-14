d6ba9a2680f3c189603b1e3bd4e9909210b30c2a
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-14 23:46:21 +0300

    add values, fields and constructor

--- src/main/java/by/itacademy/belaya/time/TimeUnitFormatter.java
+++ src/main/java/by/itacademy/belaya/time/TimeUnitFormatter.java
@@ -1,5 +1,21 @@
 package by.itacademy.belaya.time;
 
 public enum TimeUnitFormatter {
+
+    HOURS("час", "часа", "часов"),
+
+    MINUTES("минута", "минуты", "минут"),
+
+    SECONDS("секунда", "секунды", "секунд");
+
+    private final String singularForm;
+    private final String dualForm;
+    private final String pluralForm;
+
+    TimeUnitFormatter(String singularForm, String dualForm, String pluralForm) {
+        this.singularForm = singularForm;
+        this.dualForm = dualForm;
+        this.pluralForm = pluralForm;
+    }
 }
 
