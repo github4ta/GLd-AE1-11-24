c6d2645f844e7721d384a1f388dd3e7e814cd72f
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-15 00:06:07 +0300

    add method getHours

--- src/main/java/by/itacademy/belaya/time/TimeFormatter.java
+++ src/main/java/by/itacademy/belaya/time/TimeFormatter.java
@@ -1,4 +1,10 @@
 package by.itacademy.belaya.time;
 
+import java.math.BigInteger;
+
 public class TimeFormatter {
+
+    private static BigInteger getHours(long totalSeconds) {
+        return BigInteger.valueOf(totalSeconds / 3600);
+    }
 }
