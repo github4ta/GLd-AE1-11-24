99135a3b03a768bfc405aec1ae78991acb7176cd
a.maliushytski <a.maliushytski@gmail.com>
2025-01-14 17:43:11 +0100

    MOD: made changes according to the comments left

--- src/main/java/by.itacademy.maliushytski/Calendar.java
+++ src/main/java/by.itacademy.maliushytski/Calendar.java
@@ -1,66 +1,63 @@
+import java.time.Month;
+import java.time.MonthDay;
+import java.util.Set;
+
 public class Calendar {
+    private static final Set<Month> MONTHS_WITH_31_days = Set.of(
+            Month.JANUARY, Month.MARCH, Month.MAY, Month.JULY,
+            Month.AUGUST, Month.OCTOBER, Month.DECEMBER);
+
+    private static final Set<Month> MONTHS_WITH_30_days = Set.of(
+            Month.APRIL, Month.JUNE, Month.SEPTEMBER, Month.NOVEMBER);
+
+    private static final Set<MonthDay> PUBLIC_HOLIDAYS = Set.of(
+            MonthDay.of(1, 1),
+            MonthDay.of(1, 2),
+            MonthDay.of(1, 7),
+            MonthDay.of(3, 8),
+            MonthDay.of(5, 1),
+            MonthDay.of(5, 9),
+            MonthDay.of(5, 14),
+            MonthDay.of(7, 3),
+            MonthDay.of(11, 7),
+            MonthDay.of(12, 25)
+    );
+
     public static String getDayAndMonths(int day, int month) {
+        String publicHoliday = "Public holiday";
         if (day <= 0 || month <= 0) {
             throw new IllegalArgumentException("Incorrect value");
         }
 
-        Month enumMonth = Month.fromInt(month);
+        Months enumMonth = Months.fromInt(month);
+        Month javaMonth = Month.of(enumMonth.getMonthNumber());
 
-        if (month > 12 || month < 1) {
+        if (isMonthWith31DaysInvalid(javaMonth, day) || isMonthWith30DaysInvalid(javaMonth, day) ||
+                (javaMonth == Month.FEBRUARY && day > 28)) {
             return "This date does not exist";
         }
 
-        if ((enumMonth == Month.JANUARY || enumMonth == Month.MARCH || enumMonth == Month.MAY || enumMonth == Month.JULY
-                || enumMonth == Month.AUGUST || enumMonth == Month.OCTOBER || enumMonth == Month.DECEMBER) && day > 31) {
-            return "This date does not exist";
+        if (isDayPublicHoliday(javaMonth, day)) {
+            return publicHoliday;
         }
 
-        if ((enumMonth == Month.APRIL || enumMonth == Month.JUNE || enumMonth == Month.SEPTEMBER ||
-                enumMonth == Month.NOVEMBER) && day > 30) {
-            return "This date does not exist";
-        }
-
-        if (enumMonth == Month.FEBRUARY && day > 28) {
-            return "This date does not exist";
-        }
-
-        if (enumMonth == Month.JANUARY && (day == 1 || day == 2 || day == 7)) {
-            return "Public holiday";
-        }
-        if (enumMonth == Month.MARCH && day == 8) {
-            return "Public holiday";
-        }
-        if (enumMonth == Month.MAY && (day == 1 || day == 9 || day == 14)) {
-            return "Public holiday";
-        }
-        if (enumMonth == Month.JULY && day == 3) {
-            return "Public holiday";
-        }
-        if (enumMonth == Month.NOVEMBER && day == 7) {
-            return "Public holiday";
-        }
-        if (enumMonth == Month.DECEMBER && day == 25) {
-            return "Public holiday";
-        }
-        return day + getDayPostfix(day) + " of " + getFormattedMoth(enumMonth);
+        return day + DaySuffix.getSuffix(day) + " of " + getFormattedMoth(javaMonth);
     }
 
-    private static String getFormattedMoth(Month enumMonth) {
-        String monthName = enumMonth.name();
+    private static String getFormattedMoth(Month javaMonth) {
+        String monthName = javaMonth.name();
         return monthName.charAt(0) + monthName.substring(1).toLowerCase();
     }
 
-    private static String getDayPostfix(int day) {
-        String result = "";
-        if (day == 1) {
-            result = "st";
-        } else if (day == 2) {
-            result = "nd";
-        } else if (day == 3) {
-            result = "rd";
-        } else {
-            result = "th";
-        }
-        return result;
+    private static boolean isMonthWith31DaysInvalid(Month javaMonth, int day) {
+        return MONTHS_WITH_31_days.contains(javaMonth) && day > 31;
+    }
+
+    private static boolean isMonthWith30DaysInvalid(Month javaMonth, int day) {
+        return MONTHS_WITH_30_days.contains(javaMonth) && day > 30;
+    }
+
+    private static boolean isDayPublicHoliday(Month month, int day) {
+        return PUBLIC_HOLIDAYS.stream().anyMatch(h -> h.equals(MonthDay.of(Month.valueOf(month.name()), day)));
     }
 }
