0fc822c1f08e0390c0a0f7838dddf7975c7a0d8a
Nikolai <n91056379@gmail.com>
2025-01-24 21:47:55 +0300

    add xPath Field email

--- src/main/java/by/itacademy/LoginPage.java
+++ src/main/java/by/itacademy/LoginPage.java
@@ -2,6 +2,7 @@ package by.itacademy;
 
 public class LoginPage {
     public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
+    public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]";
 
 }
 
