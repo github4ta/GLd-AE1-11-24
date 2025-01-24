61a8d9ca8345d836ac752df73cadb7f3ffebaab7
Nikolai <n91056379@gmail.com>
2025-01-24 21:43:38 +0300

    add xPath Footer_Link_About_Company

--- src/main/java/by/itacademy/HomePage.java
+++ src/main/java/by/itacademy/HomePage.java
@@ -4,5 +4,5 @@ public class HomePage {
     public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
     public static final String MENU_LINK_FORUM = "//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
     public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
-
+    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\"https://blog.onliner.by/about\"]";
 }
