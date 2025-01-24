d0dbdb88552d1cc5eda7d38b547e2e764c4c804b
Nikolai <n91056379@gmail.com>
2025-01-24 21:42:20 +0300

    add xPath Footer_Copyright

--- src/main/java/by/itacademy/HomePage.java
+++ src/main/java/by/itacademy/HomePage.java
@@ -3,5 +3,6 @@ package by.itacademy;
 public class HomePage {
     public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
     public static final String MENU_LINK_FORUM = "//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
+    public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
 
 }
