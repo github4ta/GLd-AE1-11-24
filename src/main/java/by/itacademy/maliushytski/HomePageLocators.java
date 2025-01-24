d30dc8a52c9b72ec5320a1360f6d2267cd799c86
a.maliushytski <a.maliushytski@gmail.com>
2025-01-24 19:57:43 +0100

    MOD: HomePageLocators locators are updated according to the comment left in PR

--- src/main/java/by/itacademy/maliushytski/HomePageLocators.java
+++ src/main/java/by/itacademy/maliushytski/HomePageLocators.java
@@ -2,7 +2,7 @@ package by.itacademy.maliushytski;
 
 public class HomePageLocators {
     public static final String BUTTON_LOGIN = "//div[text()='Вход']";
-    public static final String MENU_LINK_FORUM = "//a[@class='b-main-navigation__link']/child::span[.='Форум']";
+    public static final String MENU_LINK_FORUM = "//a[@href='https://forum.onliner.by/']";
     public static final String FOOTER_COPYRIGHT = "//div[@class='footer-style__copy']";
-    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[normalize-space(text())='О компании']";
+    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href='https://blog.onliner.by/about']";
 }
