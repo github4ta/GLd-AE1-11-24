1351052d3d45a557fd76680294ef1a55d0022864
Nikolai <n91056379@gmail.com>
2025-01-24 21:49:17 +0300

    add xPath Button login

--- src/main/java/by/itacademy/LoginPage.java
+++ src/main/java/by/itacademy/LoginPage.java
@@ -4,6 +4,6 @@ public class LoginPage {
     public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
     public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]";
     public static final String FIELD_PASSWORD = "//input[@placeholder=\"Пароль\"]";
-
+    public static final String BUTTON_LOGIN = "//button[@class=\"auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full\"]";
 }
 
