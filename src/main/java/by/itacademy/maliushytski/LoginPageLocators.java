2bb75725c3c607e518f4f592143af5d55109791a
a.maliushytski <a.maliushytski@gmail.com>
2025-01-24 18:26:00 +0100

    ADD: added LoginPageLocators locators

---
+++ src/main/java/by/itacademy/maliushytski/LoginPageLocators.java
@@ -0,0 +1,8 @@
+package by.itacademy.maliushytski;
+
+public class LoginPageLocators {
+    public static final String TITLE_LOGIN = "//div[contains(@class,'auth-form__title')]";
+    public static final String FIELD_EMAIL = "//input[@placeholder='Ник или e-mail']";
+    public static final String FIELD_PASSWORD = "//input[@placeholder='Пароль']";
+    public static final String  BUTTON_LOGIN = "//button[@type='submit']";
+}
