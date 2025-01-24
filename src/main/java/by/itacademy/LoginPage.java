1b1793206ce06704f39d5be48bc1fa98d6876f1a
Nikolai <n91056379@gmail.com>
2025-01-24 21:47:01 +0300

    add xPath Title login

--- src/main/java/by/itacademy/LoginPage.java
+++ src/main/java/by/itacademy/LoginPage.java
@@ -1,5 +1,7 @@
 package by.itacademy;
 
 public class LoginPage {
+    public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
 
 }
+
