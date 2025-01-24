8f78f0a145e6f338f4c09a3982fd34011b50f188
Nikolai <n91056379@gmail.com>
2025-01-24 21:41:07 +0300

    add xPath Munu_Link_Forum

--- src/main/java/by/itacademy/HomePage.java
+++ src/main/java/by/itacademy/HomePage.java
@@ -2,5 +2,6 @@ package by.itacademy;
 
 public class HomePage {
     public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+    public static final String MENU_LINK_FORUM = "//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
 
 }
