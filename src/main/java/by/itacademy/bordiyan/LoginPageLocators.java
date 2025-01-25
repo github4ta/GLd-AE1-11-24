9f7eccac102cb9b9f8dbe46876c1070efa42e60c
GutenMorgen <waldstall@gmail.com>
2025-01-25 15:31:29 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
+++ src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
@@ -1,5 +1,5 @@
 package by.itacademy.bordiyan;
 
 public class LoginPageLocators {
-    public static final BUTTON_LOGIN = //div[@class="auth-bar__item auth-bar__item--text"];
+    public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
 }
