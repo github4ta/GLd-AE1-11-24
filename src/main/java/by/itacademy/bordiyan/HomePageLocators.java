6c0369f6d3ec0e13f11265121be11e1ebff34417
GutenMorgen <waldstall@gmail.com>
2025-01-25 19:58:59 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/HomePageLocators.java
+++ src/main/java/by/itacademy/bordiyan/HomePageLocators.java
@@ -1,8 +1,8 @@
 package by.itacademy.bordiyan;
 
 public class HomePageLocators {
-    public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
-    public static final String MENU_LINK_FORUM = "//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
-    public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
-    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\"https://blog.onliner.by/about\"]";
+    public static final String BUTTON_LOGIN = "//div[@class=\'auth-bar__item auth-bar__item--text\']";
+    public static final String MENU_LINK_FORUM = "//span[@class=\'b-main-navigation__text\"][text()=\"Форум\']";
+    public static final String FOOTER_COPYRIGHT = "//div[@class=\'footer-style__copy\']";
+    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\'https://blog.onliner.by/about\']";
 }
