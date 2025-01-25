2fe9edb9757594f2a862fbf15703c92ff61918b9
Nikolai <n91056379@gmail.com>
2025-01-25 08:15:56 +0300

    rename HomePageLocators

--- src/main/java/by/itacademy/HomePageLocators.java
+++ src/main/java/by/itacademy/HomePageLocators.java
@@ -1,6 +1,6 @@
 package by.itacademy;
 
-public class HomePageLocator {
+public class HomePageLocators {
     public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
     public static final String MENU_LINK_FORUM = "//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
     public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
