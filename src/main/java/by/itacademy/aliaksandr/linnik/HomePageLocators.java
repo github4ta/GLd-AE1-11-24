c1f1b6194662bd1aa2ca6dfd206d77c7a6909258
Alex Linnik <SanchezAnd3@gmail.com>
2025-01-25 09:40:57 +0300

    described locators to web elements of the main page of the site(Onliner.by)

---
+++ src/main/java/by/itacademy/aliaksandr/linnik/HomePageLocators.java
@@ -0,0 +1,9 @@
+package by.itacademy.aliaksandr.linnik;
+
+public class HomePageLocators {
+    public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+    public static final String  MENU_LINK_FORUM = "(//span[@class=\"b-main-navigation__text\"])[7]";
+    public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
+    public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\"https://blog.onliner.by/about\"]";
+
+}
