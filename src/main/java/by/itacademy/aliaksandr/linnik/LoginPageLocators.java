a3db5d211a57f679bba46aff95bac9c5c27e7312
Alex Linnik <SanchezAnd3@gmail.com>
2025-01-25 09:51:40 +0300

    described locators to web elements of the login page

---
+++ src/main/java/by/itacademy/aliaksandr/linnik/LoginPageLocators.java
@@ -0,0 +1,8 @@
+package by.itacademy.aliaksandr.linnik;
+
+public class LoginPageLocators {
+    public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
+    public static final String FIELD_EMAIL = "(//input[@class=\"auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full\"])[1]";
+    public static final String FIELD_PASSWORD = "(//input[@class=\"auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full\"])[2]";
+    public static final String BUTTON_LOGIN = "//button[@class=\"auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full\"]";
+}
