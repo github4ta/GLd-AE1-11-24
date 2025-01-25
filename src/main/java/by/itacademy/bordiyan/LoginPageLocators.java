6e678d31c0906d7b5fbe177673596e59496118c2
GutenMorgen <waldstall@gmail.com>
2025-01-26 01:15:15 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
+++ src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
@@ -1,7 +1,7 @@
 package by.itacademy.bordiyan;
 
 public class LoginPageLocators {
-    public static final String TITLE_LOGIN = "//div[@class=\'auth-bar__item auth-bar__item--text\']";
+    public static final String TITLE_LOGIN = "//div[text()='Вход']";
     public static final String FIELD_EMAIL = "//input[@placeholder=\'Ник или e-mail\']";
     public static final String FIELD_PASSWORD = "//input[@placeholder=\'Пароль\']";
     public static final String BUTTON_LOGIN = "//button[@type=\'submit\']";
