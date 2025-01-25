a1500253a26ac6c795ca0f57b2e608945effb501
Nikolai <n91056379@gmail.com>
2025-01-25 08:14:57 +0300

    rename LoginPageLocators

--- src/main/java/by/itacademy/LoginPageLocators.java
+++ src/main/java/by/itacademy/LoginPageLocators.java
@@ -1,6 +1,6 @@
 package by.itacademy;
 
-public class LoginPageLocator {
+public class LoginPageLocators {
     public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
     public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]";
     public static final String FIELD_PASSWORD = "//input[@placeholder=\"Пароль\"]";
