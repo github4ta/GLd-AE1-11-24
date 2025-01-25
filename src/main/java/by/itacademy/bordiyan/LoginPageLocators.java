2cb49409274a6c09771d6da4c010c57f2f5440e4
GutenMorgen <waldstall@gmail.com>
2025-01-25 15:59:55 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
+++ src/main/java/by/itacademy/bordiyan/LoginPageLocators.java
@@ -2,4 +2,5 @@ package by.itacademy.bordiyan;
 
 public class LoginPageLocators {
     public static final String BUTTON_LOGIN = "//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+    public static final String MENU_LINK_FORUM ="//span[@class=\"b-main-navigation__text\"][text()=\"Форум\"]";
 }
