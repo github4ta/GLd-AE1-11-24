e6b765743f8e2b1f9d6ee3640ce02cf6b3a57212
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-27 00:35:48 +0300

    add method clickButtonLogin to HomePage class

--- src/main/java/by/itacademy/belaya/HomePage.java
+++ src/main/java/by/itacademy/belaya/HomePage.java
@@ -1,6 +1,8 @@
 package by.itacademy.belaya;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
@@ -8,5 +10,12 @@ public class HomePage {
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void clickButtonLogin() {
+        String xPath = HomePageLocators.BUTTON_LOGIN;
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        webElement.click();
     }
 }
