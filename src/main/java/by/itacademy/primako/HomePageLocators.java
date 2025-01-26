d4dccf896231e0a4e5c770987866f3f08f590695
ntaranko <ntaranko@gmail.com>
2025-01-26 20:58:51 +0300

    new branch

--- src/main/java/by/itacademy/primako/HomePageLocators.java
+++
@@ -1,8 +0,0 @@
-package by.itacademy.primako;
-
-public class HomePageLocators {
-    public static final BUTTON_LOGIN ="/html/body/div[1]/div/div/div/header/div[3]/div/div[2]/div[2]/div[2]/div/div/div[1]";
-    public static final MENU_LINK_FORUM ="/html/body/div[1]/div/div/div/header/div[2]/div/nav/ul[1]/li[7]/a/span";
-    public static final FOOTER_COPYRIGHT ="/html/body/div[1]/footer/div/div/div/div[2]/div[2]";
-    public static final FOOTER_LINK_ABOUT_COMPANY ="/html/body/div[1]/footer/div/div/div/div[1]/ul/li[1]/a";
-}
