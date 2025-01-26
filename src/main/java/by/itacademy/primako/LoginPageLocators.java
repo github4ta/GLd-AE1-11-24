9a8f8b880cf536ec5c2f0bc75e2e875c162c56f0
a-primako <aleksandraprimako@gmail.com>
2025-01-26 18:32:33 +0300

    Add HomePageLocators & LoginPageLocators classes. Add locators.

---
+++ src/main/java/by/itacademy/primako/LoginPageLocators.java
@@ -0,0 +1,9 @@
+package by.itacademy.primako;
+
+public class LoginPageLocators {
+    public static final TITLE_LOGIN = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/div[1]";
+    public static final FIELD_EMAIL = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+    public static final FIELD_PASSWORD = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div/div/input";
+    public static final BUTTON_LOGIN = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[3]/button";
+
+}
