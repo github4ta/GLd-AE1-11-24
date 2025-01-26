82ca653a655d5ed3436c6eab8a15bca1c23c815c
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-27 00:57:25 +0300

    add method getTitleLoginText to LoginPage class

--- src/main/java/by/itacademy/belaya/LoginPage.java
+++ src/main/java/by/itacademy/belaya/LoginPage.java
@@ -1,6 +1,8 @@
 package by.itacademy.belaya;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
@@ -8,5 +10,14 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public String getTitleLoginText() {
+        String xPath = LoginPageLocators.TITLE_LOGIN;
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        String actual = webElement.getText();
+
+        return actual;
     }
 }
