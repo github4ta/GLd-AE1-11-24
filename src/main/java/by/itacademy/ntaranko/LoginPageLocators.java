b205225821429c4c3d94201809b9ee2b4aae45a5
ntaranko <ntaranko@gmail.com>
2025-01-26 21:35:00 +0300

    refactor TITLE_LOGIN

--- src/main/java/by/itacademy/ntaranko/LoginPageLocators.java
+++ src/main/java/by/itacademy/ntaranko/LoginPageLocators.java
@@ -1,7 +1,7 @@
 package by.itacademy.ntaranko;
 
 public class LoginPageLocators {
-    public static final String TITLE_LOGIN = "//div[@class=\"auth-form\"]"; //"xpath к тексту формы Вход"
+    public static final String TITLE_LOGIN = "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
     public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]"; //Насколько правильно искать по плейсхолдеру??
     public static final String FIELD_PASSWORD = "//input[@type=\"password\"]";
     public static final String BUTTON_LOGIN = "//button[@type=\"submit\"]";
