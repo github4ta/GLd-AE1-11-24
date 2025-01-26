341fb119650d14b7cc868c13c68e53623ded87b9
Артём <artem.kurepin999@gmail.com>
2025-01-26 20:15:12 +0300

    add methods getTitleLoginText, fillFieldEmail, fillFieldPassword, clickButtonLogin and driverQuit in class LoginPage

--- src/main/java/by/itacademy/artemkurepin/LoginPage.java
+++ src/main/java/by/itacademy/artemkurepin/LoginPage.java
@@ -1,5 +1,7 @@
 package by.itacademy.artemkurepin;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
@@ -10,10 +12,30 @@ public class LoginPage {
     }
 
     public LoginPage() {
-        this.driver=new ChromeDriver();
+        this.driver = new ChromeDriver();
     }
-    public void driverQuit(){
+
+    public void driverQuit() {
         this.driver.quit();
     }
 
+    public String getTitleLoginText() {
+        WebElement webElement = driver.findElement(By.xpath(LoginPageLocators.TITLE_LOGIN));
+        return webElement.getText();
+    }
+
+    public void fillFieldEmail(String email) {
+        WebElement webElement = driver.findElement(By.xpath(LoginPageLocators.FIELD_EMAIL));
+        webElement.sendKeys(email);
+    }
+
+    public void fillFieldPassword(String pass) {
+        WebElement webElement = driver.findElement(By.xpath(LoginPageLocators.FIELD_PASSWORD));
+        webElement.sendKeys(pass);
+    }
+
+    public void clickButtonLogin() {
+        WebElement webElement = driver.findElement(By.xpath(LoginPageLocators.BUTTON_LOGIN));
+        webElement.click();
+    }
 }
