01632b4bdae375adc206089bd7ca48647e045fb3
Nikolai <n91056379@gmail.com>
2025-01-26 16:55:41 +0300

    add HomeTest

---
+++ src/test/java/by/itacademy/HomeTest.java
@@ -0,0 +1,26 @@
+package by.itacademy;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get("https://www.onliner.by/");
+    }
+
+    @Test
+    @DisplayName("Проверка на наличие текста © 2001—2025 Onlíner")
+    public void getFooterCopyrightText() {
+        HomePage homePage = new HomePage(driver);
+        Assertions.assertEquals("© 2001—2025 Onlíner", homePage.getFooterCopyrightText());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+}
