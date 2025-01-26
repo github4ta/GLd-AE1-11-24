0742203ef303e79e8d79195bc16962572dd09f8f
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-27 00:34:47 +0300

    add WebDriver field and constructor to HomePage class

--- src/main/java/by/itacademy/belaya/HomePage.java
+++ src/main/java/by/itacademy/belaya/HomePage.java
@@ -1,4 +1,12 @@
 package by.itacademy.belaya;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
