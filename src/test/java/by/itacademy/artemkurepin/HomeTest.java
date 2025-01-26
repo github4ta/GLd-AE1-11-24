e8dde0f5247f784f4940abdd32ed4f4948a488dd
Артём <artem.kurepin999@gmail.com>
2025-01-26 20:34:10 +0300

    add test for method getFooterCopyrightText

--- src/test/java/by/itacademy/artemkurepin/HomeTest.java
+++ src/test/java/by/itacademy/artemkurepin/HomeTest.java
@@ -1,4 +1,34 @@
 package by.itacademy.artemkurepin;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomeTest {
+    public ChromeDriver driver;
+    public String actual;
+    public final String https = "https://www.onliner.by/";
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get(https);
+    }
+
+    @AfterEach
+    public void driverQuit() {
+        driver.quit();
+    }
+
+    @Test
+    public void testFooterCopyright() {
+        HomePage hp = new HomePage(driver);
+
+        actual = hp.getFooterCopyrightText();
+        Assertions.assertEquals("© 2001—2025 Onlíner", actual);
+    }
+
+
 }
