45f62398b95ae1f618a69ad3333e67692a4685dc
Артём <artem.kurepin999@gmail.com>
2025-01-27 00:22:23 +0300

    add test testGetForumTitle. Add interfaces BeforeEach and AfterEach

--- src/test/java/by/itacademy/artemkurepin/ForumTest.java
+++ src/test/java/by/itacademy/artemkurepin/ForumTest.java
@@ -1,4 +1,29 @@
 package by.itacademy.artemkurepin;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ForumTest {
+    public ChromeDriver driver;
+    public String actual;
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get("https://forum.onliner.by/");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+
+    @Test
+    public void testGetForumTitle(){
+        ForumPage fp=new ForumPage(driver);
+        actual=fp.getTitleForumText();
+        Assertions.assertEquals("Форум",actual);
+    }
 }
