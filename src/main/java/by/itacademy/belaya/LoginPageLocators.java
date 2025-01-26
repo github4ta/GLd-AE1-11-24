cff82c5be07039c80322a54362709a967d770c43
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-27 00:03:04 +0300

    refactor: replace double quotes with single quotes in XPath locator

--- src/main/java/by/itacademy/belaya/LoginPageLocators.java
+++ src/main/java/by/itacademy/belaya/LoginPageLocators.java
@@ -2,9 +2,9 @@ package by.itacademy.belaya;
 
 public class LoginPageLocators {
     public static final String TITLE_LOGIN =
-            "//div[@class=\"auth-form__title auth-form__title_big auth-form__title_condensed-default\"]";
-    public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]";
-    public static final String FIELD_PASSWORD = "//input[@placeholder=\"Пароль\"]";
+            "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String FIELD_EMAIL = "//input[@placeholder='Ник или e-mail']";
+    public static final String FIELD_PASSWORD = "//input[@placeholder='Пароль']";
     public static final String BUTTON_LOGIN = "//button[@type='submit']";
 }
 
