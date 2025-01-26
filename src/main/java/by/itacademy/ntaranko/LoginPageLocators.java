d8b6b4e794cd01efb7539ba7300bf8f3c6695da8
ntaranko <ntaranko@gmail.com>
2025-01-26 21:23:06 +0300

    add LoginPageLocators.java

---
+++ src/main/java/by/itacademy/ntaranko/LoginPageLocators.java
@@ -0,0 +1,9 @@
+package by.itacademy.ntaranko;
+
+public class LoginPageLocators {
+    public static final String TITLE_LOGIN = "//div[@class=\"auth-form\"]"; //"xpath к тексту формы Вход"
+    public static final String FIELD_EMAIL = "//input[@placeholder=\"Ник или e-mail\"]"; //Насколько правильно искать по плейсхолдеру??
+    public static final String FIELD_PASSWORD = "//input[@type=\"password\"]";
+    public static final String BUTTON_LOGIN = "//button[@type=\"submit\"]";
+}
+//div[@class="auth-input__wrapper auth-form__input-wrapper auth-form__input-wrapper_width_full"]
