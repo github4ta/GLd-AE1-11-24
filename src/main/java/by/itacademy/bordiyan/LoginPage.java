db42dd25ea09b7cf12ffd6bd9199a3b09d5bf5d6
GutenMorgen <waldstall@gmail.com>
2025-01-26 21:22:08 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/LoginPage.java
+++ src/main/java/by/itacademy/bordiyan/LoginPage.java
@@ -1,4 +1,21 @@
 package by.itacademy.bordiyan;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public String getTitleLoginText(){
+        String xPath = "//div[@class=\'auth-bar__item auth-bar__item--text\'";
+        WebElement webElement= driver.findElement(By.xpath(xPath));
+        return webElement.getText();
+    }
+
+
 }
