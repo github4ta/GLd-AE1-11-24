5d6ee5d1eca5da8ac4d559957a408ebcac978737
GutenMorgen <waldstall@gmail.com>
2025-01-26 21:26:07 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/LoginPage.java
+++ src/main/java/by/itacademy/bordiyan/LoginPage.java
@@ -7,15 +7,32 @@ import org.openqa.selenium.WebElement;
 public class LoginPage {
     private WebDriver driver;
 
-    public LoginPage(WebDriver driver){
+    public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public String getTitleLoginText(){
+    public String getTitleLoginText() {
         String xPath = "//div[@class=\'auth-bar__item auth-bar__item--text\'";
-        WebElement webElement= driver.findElement(By.xpath(xPath));
+        WebElement webElement = driver.findElement(By.xpath(xPath));
         return webElement.getText();
     }
 
+    public void fillFieldEmail(String email) {
+        String xPath = "//input[@placeholder=\'Ник или e-mail\']";
+        WebElement webElement = driver.findElement(By.xpath(xPath));
+        webElement.sendKeys(email);
+    }
 
+    public void fillFieldPassword(String password) {
+        String xPath = "//input[@placeholder=\'Пароль\']";
+        WebElement webElement = driver.findElement(By.xpath(xPath));
+        webElement.sendKeys(password);
+    }
+
+    public void clickButtonLogin() {
+        String xPath = "//button[@type=\'submit\']";
+        WebElement loginButton = driver.findElement(By.xpath(xPath));
+        loginButton.click();
+    }
 }
+
