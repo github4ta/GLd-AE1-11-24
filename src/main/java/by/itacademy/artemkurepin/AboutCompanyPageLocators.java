0b15979f34de482efc8c3789476d87c29714c96f
Артём <artem.kurepin999@gmail.com>
2025-01-27 00:09:10 +0300

    add var TITLE_ABOUT_COMPANY

--- src/main/java/by/itacademy/artemkurepin/AboutCompanyPageLocators.java
+++ src/main/java/by/itacademy/artemkurepin/AboutCompanyPageLocators.java
@@ -1,4 +1,6 @@
 package by.itacademy.artemkurepin;
 
 public class AboutCompanyPageLocators {
+    public static final String TITLE_ABOUT_COMPANY =
+            "/html/body/div[1]/div/div/div[2]/div/div/div[3]/div[1]/div/div/div[2]/div/div[1]/div/h1";
 }
