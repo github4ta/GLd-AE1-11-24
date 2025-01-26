f63c2e93f5f8524f3e029717da1be6b374261c71
ntaranko <ntaranko@gmail.com>
2025-01-26 18:46:33 +0300

    refactor methods

--- src/main/java/by/itcademy/PositiveNumbers.java
+++ src/main/java/by/itcademy/PositiveNumbers.java
@@ -2,30 +2,41 @@ package by.itcademy;
 
 public class PositiveNumbers {
     public static String positiveNumbers(int a, int b, int c) {
-        if (a > 0 && b > 0 && c > 0) {
+        if (isAllNumbersPositive(a, b, c)) {
             return "excellent";
         }
 
-        if (a * b < 0 && c > 0 ||
-                a * c < 0 && b > 0 ||
-                c * b < 0 && a > 0 ||
-                a == 0 && b > 0 && c > 0 ||
-                b == 0 && a > 0 && c > 0 ||
-                c == 0 && b > 0 && a > 0
-        ) {
+        if (isAnyOfThreeNumbersPositive(a, b, c)) {
             return "good";
         }
 
-        if (a * b < 0 && c < 0 ||
-                a * c < 0 && b < 0 ||
-                c * b < 0 && a < 0 ||
-                a == 0 && b == 0 && c > 0 ||
-                b == 0 && c == 0 && a > 0 ||
-                c == 0 && a == 0 && b > 0) {
+        if (isAllNumbersNegative(a, b, c)) {
             return "fine";
         } else {
             return "bad";
         }
+    }
+
+    private static boolean isAllNumbersPositive(int a, int b, int c) {
+        return a > 0 && b > 0 && c > 0;
+    }
+
+    private static boolean isAnyOfThreeNumbersPositive(int a, int b, int c) {
+        return (a * b < 0 && c > 0 ||
+                a * c < 0 && b > 0 ||
+                c * b < 0 && a > 0 ||
+                a == 0 && b > 0 && c > 0 ||
+                b == 0 && a > 0 && c > 0 ||
+                c == 0 && b > 0 && a > 0);
+    }
+
+    private static boolean isAllNumbersNegative(int a, int b, int c) {
+        return (a * b < 0 && c < 0 ||
+                a * c < 0 && b < 0 ||
+                c * b < 0 && a < 0 ||
+                a == 0 && b == 0 && c > 0 ||
+                b == 0 && c == 0 && a > 0 ||
+                c == 0 && a == 0 && b > 0);
     }
 }
 
