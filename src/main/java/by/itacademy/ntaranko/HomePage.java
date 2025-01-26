c4448f8356f7211fb1767e9897e3f9b89d39fdad
ntaranko <ntaranko@gmail.com>
2025-01-26 22:49:51 +0300

    refactor HomePage.java

--- src/main/java/by/itacademy/ntaranko/HomePage.java
+++ src/main/java/by/itacademy/ntaranko/HomePage.java
@@ -2,7 +2,6 @@ package by.itacademy.ntaranko;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
 
@@ -13,22 +12,18 @@ public class HomePage {
     }
 
     public void clickButtonLogin() {
-        driver = new ChromeDriver();
         driver.findElement(By.xpath(HomePageLocators.BUTTON_LOGIN));
     }
 
     public void clickMenuLinkForum() {
-        driver = new ChromeDriver();
         driver.findElement(By.xpath(HomePageLocators.MENU_LINK_FORUM));
     }
 
     public String getFooterCopyrightText() {
-        driver = new ChromeDriver();
         return driver.findElement(By.xpath(HomePageLocators.FOOTER_COPYRIGHT)).getText();
     }
 
     public void clickFooterLinkAboutCompany() {
-        driver = new ChromeDriver();
         driver.findElement(By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY));
     }
 
