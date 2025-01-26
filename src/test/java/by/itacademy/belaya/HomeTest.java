23aaaa29c9886444d3b7f73fe33a164185bd2e74
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-01-27 01:31:51 +0300

    add method beforeEach to HomeTest

--- src/test/java/by/itacademy/belaya/HomeTest.java
+++ src/test/java/by/itacademy/belaya/HomeTest.java
@@ -1,8 +1,16 @@
 package by.itacademy.belaya;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomeTest {
 
     WebDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get("https://www.onliner.by/");
+    }
 }
