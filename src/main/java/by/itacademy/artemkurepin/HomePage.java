a9c036802b156507024513c64e0e7bbaa6471816
Артём <artem.kurepin999@gmail.com>
2025-01-26 20:10:03 +0300

    add class HomePage

---
+++ src/main/java/by/itacademy/artemkurepin/HomePage.java
@@ -0,0 +1,39 @@
+package by.itacademy.artemkurepin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    private ChromeDriver driver;
+
+
+    public HomePage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    public HomePage() {
+        this.driver = new ChromeDriver();
+    }
+
+    public void clickButtonLogin() {
+        WebElement webElement=driver.findElement(By.xpath(HomePageLocators.BUTTON_LOGIN));
+        webElement.click();
+    }
+    public void clickMenuLinkForum(){
+        WebElement webElement=driver.findElement(By.xpath(HomePageLocators.MENU_LINK_FORUM));
+        webElement.click();
+    }
+    public String getFooterCopyrightText(){
+        WebElement webElement=driver.findElement(By.xpath(HomePageLocators.MENU_LINK_FORUM));
+        return webElement.getText();
+    }
+    public void clickFooterLinkAboutCompany(){
+        WebElement webElement=driver.findElement(By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY));
+        webElement.click();
+    }
+    public void driverQuit(){
+        this.driver.quit();
+    }
+}
