480571248b2412b93b7bd0edb322586c2a6704b6
a-primako <aleksandraprimako@gmail.com>
2025-01-26 19:20:57 +0300

    Add HomePage & LoginPage classes. Add methods.

---
+++ src/main/java/by/itacademy/primako/HomePage.java
@@ -0,0 +1,42 @@
+package by.itacademy.primako;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    private ChromeDriver driver;
+
+    public HomePage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickButtonLogin() {
+        String xPath = "/html/body/div[1]/div/div/div/header/div[3]/div/div[2]/div[2]/div[2]/div/div/div[1]";
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        webElement.click();
+    }
+
+    public void clickMenuLinkForum() {
+        String xPath = "/html/body/div[1]/div/div/div/header/div[2]/div/nav/ul[1]/li[7]/a/span";
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        webElement.click();
+    }
+
+    public getFooterCopyrightText() {
+        String xPath = "/html/body/div[1]/footer/div/div/div/div[2]/div[2]";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        String actual = webElement.getText();
+        return actual;
+    }
+
+    public void clickFooterLinkAboutCompany() {
+        String xPath = "/html/body/div[1]/footer/div/div/div/div[1]/ul/li[1]/a";
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        webElement.click();
+    }
+}
---
+++ src/main/java/by/itacademy/primako/LoginPage.java
@@ -0,0 +1,41 @@
+package by.itacademy.primako;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    private ChromeDriver driver;
+
+    public LoginPage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTitleLoginText() {
+        String xPath = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/div[1]";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        String actual = webElement.getText();
+        return actual;
+    }
+
+    public void fillFieldEmail(String email) {
+        String xPath = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.sendKeys(email);
+    }
+
+    public void fillFieldPassword(String password) {
+        String xPath = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div/div/input";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.sendKeys(password);
+    }
+
+    public void clickButtonLogin() {
+        String xPath = "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div/form/div[3]/button";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.click();
+}
