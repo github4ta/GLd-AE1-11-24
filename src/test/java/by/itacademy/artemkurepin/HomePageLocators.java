d284c81ccc4bc84e33a9ddc3a23f24f47639eb1a
Артём <artem.kurepin999@gmail.com>
2025-01-26 19:54:19 +0300

    add and fill class HomePageLocators and class LoginPageLocators

---
+++ src/test/java/by/itacademy/artemkurepin/HomePageLocators.java
@@ -0,0 +1,8 @@
+package by.itacademy.artemkurepin;
+
+public class HomePageLocators {
+    public static final String  BUTTON_LOGIN="//div[@class=\"auth-bar__item auth-bar__item--text\"]";
+    public static final String MENU_LINK_FORUM="//a[@href=\"https://forum.onliner.by/\"]";
+    public static final String FOOTER_COPYRIGHT="//div[@class=\"footer-style__copy\"]";
+    public static final String FOOTER_LINK_ABOUT_COMPANY="//a[@href=\"https://blog.onliner.by/about\"]";
+}
