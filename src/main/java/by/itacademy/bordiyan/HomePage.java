19b2d216eab72d24f8223d8cd18d8048ecdc63a8
GutenMorgen <waldstall@gmail.com>
2025-01-26 20:59:40 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/HomePage.java
+++ src/main/java/by/itacademy/bordiyan/HomePage.java
@@ -3,16 +3,16 @@ package by.itacademy.bordiyan;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
+    private WebDriver driver;
 
-    public HomePage(ChromeDriver driver) {
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-
-    private final ChromeDriver driver;
 
     @BeforeEach
     public void beforeEach() {
@@ -28,14 +28,25 @@ public class HomePage {
     }
 
     public void clickMenuLinkForum() {
-        String xPath = "//div[text()='Вход']";
+        String xPath = "//span[@class=\'b-main-navigation__text\"][text()=\'Форум\']";
         By byXpath = By.xpath(xPath);
         WebElement webElement = driver.findElement(byXpath);
         webElement.click();
     }
 
+    public void getFooterCopyrightText() {
+        String xPath = "//div[@class=\'footer-style__copy\']";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.click();
+    }
 
-
+    public void clickMenuLinkForum() {
+        String xPath = "//a[@href=\'https://blog.onliner.by/about\']";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.click();
+    }
 
     @AfterEach
     public void afterEach() {
