29fc011f0d53141294fb5fd21d19e2dd8b2d7802
ntaranko <ntaranko@gmail.com>
2025-01-26 18:53:01 +0300

    refactor - add methods

--- src/main/java/by/itcademy/SumNumbersFromDigits.java
+++ src/main/java/by/itcademy/SumNumbersFromDigits.java
@@ -2,9 +2,16 @@ package by.itcademy;
 
 public class SumNumbersFromDigits {
     public static int sumFromDigits(int number) {
-
-        int ab = number / 100;
-        int cd = number % 100;
+        int ab = getABNumber(number);
+        int cd = getCDNumber(number);
         return ab + cd;
+    }
+
+    private static int getABNumber(int number){
+        return number / 100;
+    }
+
+    private static int getCDNumber(int number){
+        return number % 100;
     }
 }
