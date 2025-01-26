17cc3db2f06164eb5e01adc454a9495a9cba8d5e
ntaranko <ntaranko@gmail.com>
2025-01-26 22:50:14 +0300

    add HomeTest.java

---
+++ src/test/java/by/itacademy/ntaranko/HomeTest.java
@@ -0,0 +1,30 @@
+package by.itacademy.ntaranko;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void start(){
+        driver = new ChromeDriver( );
+        driver.get("https://www.onliner.by/");
+
+    }
+
+    @Test
+    @DisplayName("footer copyright contains \"© 2001—2025 Onlíner\"")
+    public void test1() {
+        HomePage homePage = new HomePage(driver);
+        String result = homePage.getFooterCopyrightText();
+
+        Assertions.assertEquals("© 2001—2025 Onlíner", result);
+    }
+
+    @AfterEach
+    public void end(){
+        driver.quit();
+    }
+}
