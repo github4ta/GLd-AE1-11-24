48d3c3995b5bbdb86d64fb8252c3b9a55f7f150f
GutenMorgen <waldstall@gmail.com>
2025-01-26 16:35:22 +0300

    add solution

--- src/main/java/by/itacademy/bordiyan/HomePage.java
+++ src/main/java/by/itacademy/bordiyan/HomePage.java
@@ -1,4 +1,29 @@
 package by.itacademy.bordiyan;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
+
+    public WtPage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    private ChromeDriver driver;
+
+    public void clickButtonLogin() {
+        String xPath = "//div[text()='Вход']";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.click();
+    }
+
+    public void clickMenuLinkForum() {
+        String xPath = "//div[text()='Вход']";
+        By byXpath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXpath);
+        webElement.click();
+    }
+
 }
