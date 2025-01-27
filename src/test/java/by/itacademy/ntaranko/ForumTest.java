8ec75f380bbc2b7616a0f69186234116cd332786
ntaranko <ntaranko@gmail.com>
2025-01-27 12:38:55 +0300

    add ForumTest

---
+++ src/test/java/by/itacademy/ntaranko/ForumTest.java
@@ -0,0 +1,33 @@
+package by.itacademy.ntaranko;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ForumTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void start(){
+        driver = new ChromeDriver( );
+        driver.get("https://www.onliner.by/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    @DisplayName("forum contains Форум in header")
+    public void test1() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickMenuLinkForum();
+
+        ForumPage forumPage = new ForumPage(driver);
+        String result = forumPage.getTitleForumText();
+
+        Assertions.assertEquals("Форум", result);
+    }
+
+    @AfterEach
+    public void end(){
+        driver.quit();
+    }
+}
