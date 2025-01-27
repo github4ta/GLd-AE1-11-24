1e4cf9af75cc96d8be4a9afb1b4aabf69b36d36d
a.maliushytski <a.maliushytski@gmail.com>
2025-01-27 17:34:58 +0100

    MOD: changes in clickFooterLinkAboutCompany method

--- src/main/java/by/itacademy/maliushytski/HomePage.java
+++ src/main/java/by/itacademy/maliushytski/HomePage.java
@@ -1,14 +1,21 @@
 package by.itacademy.maliushytski;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public HomePage clickButtonLogin() {
@@ -34,12 +41,17 @@ public class HomePage {
     public HomePage clickFooterLinkAboutCompany() {
         By footerAboutCompanyLink = By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY);
         WebElement menuLinkElement = driver.findElement(footerAboutCompanyLink);
-        menuLinkElement.click();
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(footerAboutCompanyLink));
+            wait.until(ExpectedConditions.elementToBeClickable(footerAboutCompanyLink));
+            menuLinkElement.click();
+        } catch (StaleElementReferenceException e) {
+            driver.navigate().refresh();
+            By locator = By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY);
+            WebElement element = driver.findElement(locator);
+            element.click();
+        }
         return this;
-    }
-
-    public static By returnByLocator(String locator) {
-        return By.xpath(locator);
     }
 
     public HomePage clickAcceptCookies() {
--- src/test/java/by/itacademy/maliushytski/AboutCompanyTest.java
+++ src/test/java/by/itacademy/maliushytski/AboutCompanyTest.java
@@ -1,19 +1,11 @@
 import by.itacademy.maliushytski.AboutCompanyPage;
 import by.itacademy.maliushytski.HomePage;
-import by.itacademy.maliushytski.HomePageLocators;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.Objects;
 
 public class AboutCompanyTest {
     private WebDriver driver;
-    private WebDriverWait wait;
     public HomePage homePage;
     private AboutCompanyPage aboutCompanyPage;
 
@@ -21,26 +13,16 @@ public class AboutCompanyTest {
     public void beforeEach() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://www.onliner.by/");
         homePage = new HomePage(driver);
         aboutCompanyPage = new AboutCompanyPage(driver);
     }
 
     @Test
-    public void testAboutCompanyPageTitle() throws InterruptedException {
+    public void testAboutCompanyPageTitle() {
         String expectedAboutCompanyPageTitle = "Информация о компании";
-        homePage.clickAcceptCookies();
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(HomePage.returnByLocator(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY)));
-            wait.until(ExpectedConditions.elementToBeClickable(HomePage.returnByLocator(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY)));
-            homePage.clickFooterLinkAboutCompany();
-        } catch (StaleElementReferenceException e) {
-            driver.navigate().refresh();
-            By locator = By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY);
-            WebElement element = driver.findElement(locator);
-            element.click();
-        }
+        homePage.clickAcceptCookies()
+                .clickFooterLinkAboutCompany();
         Assertions.assertEquals(expectedAboutCompanyPageTitle, aboutCompanyPage.getTitleAboutCompanyText());
     }
 
