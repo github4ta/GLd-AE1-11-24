a64009a26f0ae3dadfa0bce1343d4c0e6edec9c3
Nikolai <n91056379@gmail.com>
2025-01-27 15:53:09 +0300

    refactor xPath AboutCompany

--- src/main/java/by/itacademy/AboutCompanyPageLocators.java
+++ src/main/java/by/itacademy/AboutCompanyPageLocators.java
@@ -1,5 +1,5 @@
 package by.itacademy;
 
 public class AboutCompanyPageLocators {
-    public static final String TITLE_ABOUT_COMPANY= "//h1[text()=\"Информация о компании\"]";
+    public static final String TITLE_ABOUT_COMPANY = "(//a[@class=\"footer-style__link footer-style__link_primary\"])[1]";
 }
