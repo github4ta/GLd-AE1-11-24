399c42cb75ca8cf5c7fef53bbe94133fe0dc4119
Nikolai <n91056379@gmail.com>
2025-01-27 14:24:42 +0300

    add method getTitleForumText

--- src/main/java/by/itacademy/ForumPage.java
+++ src/main/java/by/itacademy/ForumPage.java
@@ -1,5 +1,21 @@
 package by.itacademy;
 
-public class ForumPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class ForumPage {
+    private ChromeDriver driver;
+
+    public ForumPage(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTitleForumText() {
+        String xPath = "//a[@href=\"https://forum.onliner.by\"][text()=\"Форум\"]";
+        By byXPath = By.xpath(xPath);
+        WebElement webElement = driver.findElement(byXPath);
+        String actual = webElement.getText();
+        return actual;
+    }
 }
