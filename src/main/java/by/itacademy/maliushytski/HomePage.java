542601c8bb69ae4dc09a06cbc60b99a27bd59a62
a.maliushytski <a.maliushytski@gmail.com>
2025-01-27 14:26:16 +0100

    ADD: HomePage and LoginPage are added

---
+++ src/main/java/by/itacademy/maliushytski/HomePage.java
@@ -0,0 +1,41 @@
+package by.itacademy.maliushytski;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        driver = new ChromeDriver();
+    }
+
+    public HomePage clickButtonLogin() {
+        By loginButton = By.xpath(HomePageLocators.BUTTON_LOGIN);
+        WebElement loginButtonElement = driver.findElement(loginButton);
+        loginButtonElement.click();
+        return this;
+    }
+
+    public HomePage clickMenuLinkForum() {
+        By menuLink = By.xpath(HomePageLocators.MENU_LINK_FORUM);
+        WebElement menuLinkElement = driver.findElement(menuLink);
+        menuLinkElement.click();
+        return this;
+    }
+
+    public String getFooterCopyrightText() {
+        By footerCopyright = By.xpath(HomePageLocators.FOOTER_COPYRIGHT);
+        WebElement footerCopyrightElement = driver.findElement(footerCopyright);
+        return footerCopyrightElement.getText();
+    }
+
+    public HomePage clickFooterLinkAboutCompany() {
+        By footerAboutCompanyLink = By.xpath(HomePageLocators.FOOTER_LINK_ABOUT_COMPANY);
+        WebElement menuLinkElement = driver.findElement(footerAboutCompanyLink);
+        menuLinkElement.click();
+        return this;
+    }
+}
---
+++ src/main/java/by/itacademy/maliushytski/LoginPage.java
@@ -0,0 +1,41 @@
+package by.itacademy.maliushytski;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        driver = new ChromeDriver();
+    }
+
+    public String getTitleLoginText() {
+        By titleLoginText = By.xpath(LoginPageLocators.TITLE_LOGIN);
+        WebElement titleLoginTextElement = driver.findElement(titleLoginText);
+        return titleLoginTextElement.getText();
+    }
+
+    public LoginPage fillFieldEmail(String email) {
+        By emailField = By.xpath(LoginPageLocators.FIELD_EMAIL);
+        WebElement emailFieldElement = driver.findElement(emailField);
+        emailFieldElement.sendKeys(email);
+        return this;
+    }
+
+    public LoginPage fillFieldPassword(String password) {
+        By passwordField = By.xpath(LoginPageLocators.FIELD_PASSWORD);
+        WebElement passwordFieldElement = driver.findElement(passwordField);
+        passwordFieldElement.sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickButtonLogin() {
+        By loginButton = By.xpath(LoginPageLocators.BUTTON_LOGIN);
+        WebElement loginButtonElement = driver.findElement(loginButton);
+        loginButtonElement.click();
+        return this;
+    }
+}
