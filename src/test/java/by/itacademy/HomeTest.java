b1326a0197714413bfdcdd19d22936e4d390c827
Nikolai <n91056379@gmail.com>
2025-01-27 17:43:59 +0300

    refactor HomeTest

--- src/test/java/by/itacademy/HomeTest.java
+++ src/test/java/by/itacademy/HomeTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomeTest {
-    WebDriver driver;
+   private WebDriver driver;
 
     @BeforeEach
     public void beforeEach() {
