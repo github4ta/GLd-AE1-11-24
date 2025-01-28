267caf636f09420eedeab67dfbc78403ad1a98a3
ntaranko <ntaranko@gmail.com>
2025-01-28 20:49:25 +0300

    refactor - make driver private

--- src/test/java/by/itacademy/ntaranko/ForumTest.java
+++ src/test/java/by/itacademy/ntaranko/ForumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ForumTest {
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeEach
     public void start(){
