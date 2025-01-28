419c60cac25888a0f7ede275df8a9b7d91558578
GutenMorgen <waldstall@gmail.com>
2025-01-28 19:25:18 +0300

    add solution

---
+++ src/main/java/by/itacademy/bordiyan/CartPage.java
@@ -0,0 +1,16 @@
+package by.itacademy.bordiyan;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    public WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getYourArtIsEmptyText(){
+
+    }
+
+}
--- src/main/java/by/itacademy/bordiyan/CartPageLocators.java
+++ src/main/java/by/itacademy/bordiyan/CartPageLocators.java
@@ -3,5 +3,5 @@ package by.itacademy.bordiyan;
 import org.openqa.selenium.By;
 
 public class CartPageLocators {
-     public final By YOR_ART_IS_EMTY_TEXT =By.xpath("//div[@class='cart-message__title cart-message__title_big']");
+     public static final By YOUR_ART_IS_EMPTY_TEXT =By.xpath("//div[@class='cart-message__title cart-message__title_big']");
 }
