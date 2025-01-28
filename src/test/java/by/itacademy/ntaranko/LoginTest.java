2ab05c0960149a2964f41b813af570bc89c4b2d8
ntaranko <ntaranko@gmail.com>
2025-01-28 20:51:25 +0300

    refactor - make driver private

--- src/test/java/by/itacademy/ntaranko/LoginTest.java
+++ src/test/java/by/itacademy/ntaranko/LoginTest.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeEach
     public void start() {
