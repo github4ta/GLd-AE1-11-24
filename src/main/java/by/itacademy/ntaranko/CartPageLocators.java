d1045960fed33ace784442b2a32beb23b907de4e
ntaranko <ntaranko@gmail.com>
2025-01-28 20:58:26 +0300

    add CartPageLocators

---
+++ src/main/java/by/itacademy/ntaranko/CartPageLocators.java
@@ -0,0 +1,5 @@
+package by.itacademy.ntaranko;
+
+public class CartPageLocators {
+    public static final String YOUR_CARD_IS_EMPTY_TEXT = "//div[@class=\"cart-message__title cart-message__title_big\"]";
+}
