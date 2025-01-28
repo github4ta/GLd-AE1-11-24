82e40e048ad6c28aaab96aed125426607b228b44
ntaranko <ntaranko@gmail.com>
2025-01-28 20:57:02 +0300

    refactor - add locator for cart

--- src/main/java/by/itacademy/ntaranko/HomePageLocators.java
+++ src/main/java/by/itacademy/ntaranko/HomePageLocators.java
@@ -5,4 +5,5 @@ public class HomePageLocators {
     public static final String MENU_LINK_FORUM = "//a[@href=\"https://forum.onliner.by/\"]";
     public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
     public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\"https://blog.onliner.by/about\"]";
+    public static final String BUTTON_CART = "//a[@title=\"Корзина\"]";
 }
