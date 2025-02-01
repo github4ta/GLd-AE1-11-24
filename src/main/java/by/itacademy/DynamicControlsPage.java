e4de9c9b7cc5f1bec4ebff9c926de7754d166fc7
GutenMorgen <waldstall@gmail.com>
2025-02-01 20:19:03 +0300

    add solution

--- src/main/java/by/itacademy/DynamicControlsPage.java
+++ src/main/java/by/itacademy/DynamicControlsPage.java
@@ -1,6 +1,15 @@
 package by.itacademy;
 
-public class DynamicControlsPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+public class DynamicControlsPage {
+    private WebDriver driver;
+
+    public void toggleCheckbox(){
+    WebElement toggleButton = ExpectedConditions.elementToBeClickable(Locators.toggleCheckboxButton);
+        toggleButton.click();
+}
 
 }
