87edd1096300766b12dd2e99e4dde129fae26782
GutenMorgen <waldstall@gmail.com>
2025-02-01 20:02:59 +0300

    add solution

---
+++ src/main/java/by/itacademy/Locators.java
@@ -0,0 +1,13 @@
+package by.itacademy;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+    public static class LocatorPage {
+        public static By CHECKBOX = By.id("checkbox");
+        public static By TOGGLE_CHECK_BOX_BUTTON_REMOVE = By.id("//button[text()=\'Remove\']");
+        public static By MESSAGE = By.id("message");
+        public static By INPUT_FIELD = By.id("//button[text()=\'Remove\']");
+        public static By TOGGLE_INPUT_BUTTON = By.xpath("//button[text()='Enable']");
+    }
+}
