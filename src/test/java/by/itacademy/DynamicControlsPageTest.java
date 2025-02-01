4aacc6913b542f4b72e987985f4fc5e4d91ae325
GutenMorgen <waldstall@gmail.com>
2025-02-01 22:51:03 +0300

    add solution

--- src/test/java/by/itacademy/DynamicControlsPageTest.java
+++ src/test/java/by/itacademy/DynamicControlsPageTest.java
@@ -3,45 +3,33 @@ package by.itacademy;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DynamicControlsTest {
+public class DynamicControlsPageTest {
     private WebDriver driver;
     private DynamicControlsPage dynamicControlsPage;
 
     @BeforeClass
-    public void setUp() {
-        // Установите путь к вашему драйверу (например, ChromeDriver)
+    public void beforeClass() {
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-
-        // Инициализация драйвера
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-
         driver.get("https://the-internet.herokuapp.com/dynamic_controls");
         dynamicControlsPage = new DynamicControlsPage(driver);
     }
 
     @Test()
     public void testCheckboxRemoval() {
-        // Убираем чекбокс
         dynamicControlsPage.toggleCheckbox();
-
-
-        Assert.assertTrue(dynamicControlsPage.isCheckboxDisplayed(), "Чекбокс не исчез!");
-
-
-        Assert.assertEquals(dynamicControlsPage.getMessageText(), "It's gone!", "Сообщение не совпадает!");
+        Assertions.assertTrue(dynamicControlsPage.isCheckboxDisplayed(), "Чекбокс не исчез!");
+        Assertions.assertEquals(dynamicControlsPage.getMessageText(), "It's gone!", "Сообщение не совпадает!");
     }
 
-
-
     @AfterClass
-    public void tearDown() {
-        // Закрываем браузер
+    public void afterClass() {
         if (driver != null) {
             driver.quit();
         }
