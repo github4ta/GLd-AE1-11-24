f48df9c848b5d7a9356f7a1ac8aec1b1e2a2957a
GutenMorgen <waldstall@gmail.com>
2025-02-01 19:35:47 +0300

    add solution

--- src/test/java/by/itacademy/LocatorPage.java
+++ src/test/java/by/itacademy/LocatorPage.java
@@ -3,5 +3,7 @@ package by.itacademy;
 import org.openqa.selenium.By;
 
 public class LocatorPage {
-    public static By checkboks = By.id("checkbox");
+    public static By CHECKBOX = By.id("checkbox");
+    public static By TOGGLE_CHECK_BOX_BUTTON_REMOVE = By.id("//button[text()=\'Remove\']");
+
 }
