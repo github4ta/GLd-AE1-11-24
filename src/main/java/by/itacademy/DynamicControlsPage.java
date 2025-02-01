5381643f423200327bcc293eba515dce240c9b15
GutenMorgen <waldstall@gmail.com>
2025-02-01 22:10:27 +0300

    add solution

--- src/main/java/by/itacademy/DynamicControlsPage.java
+++ src/main/java/by/itacademy/DynamicControlsPage.java
@@ -1,8 +1,9 @@
 package by.itacademy;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 
@@ -13,6 +14,11 @@ public class DynamicControlsPage {
     public DynamicControlsPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void toggleCheckbox() {
+        WebElement toggleButton = wait.until(ExpectedConditions.elementToBeClickable(Locators.TOGGLE_CHECK_BOX_BUTTON_REMOVE));
+        toggleButton.click();
     }
 }
 
--- src/test/java/by/itacademy/DynamicControlsPageTest.java
+++ src/test/java/by/itacademy/DynamicControlsPageTest.java
@@ -6,34 +6,33 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-    public class DynamicControlsTest {
-        private WebDriver driver;
-        private DynamicControlsPage dynamicControlsPage;
+public class DynamicControlsTest {
+    private WebDriver driver;
+    private DynamicControlsPage dynamicControlsPage;
 
-        @BeforeClass
-        public static void setUpInternet() {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-            dynamicControlsPage = new DynamicControlsPage(driver);
-        }
+    @BeforeClass
+    public static void setUpInternet() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        dynamicControlsPage = new DynamicControlsPage(driver);
+    }
 
-        @Test(priority = 1)
-        public void testCheckboxRemoval() {
+    @Test(priority = 1)
+    public void testCheckboxRemoval() {
 
-            dynamicControlsPage.toggleCheckbox();
-
-
-            Assert.assertTrue(dynamicControlsPage.isCheckboxDisplayed(), "Чекбокс не исчез!");
+        dynamicControlsPage.toggleCheckbox();
 
 
-            Assert.assertEquals(dynamicControlsPage.getMessageText(), "It's gone!", "Сообщение не совпадает!");
-        }
+        Assert.assertTrue(dynamicControlsPage.isCheckboxDisplayed(), "Чекбокс не исчез!");
 
 
-        @AfterEach
-        public void afterEach() {
-            driver.quit();
-        }
+        Assert.assertEquals(dynamicControlsPage.getMessageText(), "It's gone!", "Сообщение не совпадает!");
+    }
+
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
     }
 }
