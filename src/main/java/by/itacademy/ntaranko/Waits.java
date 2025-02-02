94f2f0656d8ceb8f6444975c52d6436dcc63d421
ntaranko <ntaranko@gmail.com>
2025-02-02 21:53:11 +0300

    add Waits.java

---
+++ src/main/java/by/itacademy/ntaranko/Waits.java
@@ -0,0 +1,11 @@
+package by.itacademy.ntaranko;
+
+public class Waits {
+    public static void waitFor(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
