cb751f02a957342001202cb2cceb339d3da7e97c
Nikolai <n91056379@gmail.com>
2025-02-02 17:42:56 +0300

    add DynamicLoadingLocators

---
+++ src/main/java/by/itacademy/DynamicLoadingLocators.java
@@ -0,0 +1,10 @@
+package by.itacademy;
+
+import org.openqa.selenium.By;
+
+public class DynamicLoadingLocators {
+    public static final By BUTTON_START1 = By.xpath("//button[text()=\"Start\"]");
+    public static final By HELLO_WORLD1 = By.xpath("//h4[text()=\"Hello World!\"]");
+    public static final By BUTTON_START2 = By.xpath("//button[text()=\"Start\"]");
+    public static final By HELLO_WORLD2 = By.xpath("//h4[text()=\"Hello World!\"]");
+}
