52ee53359392e1d66b637bdb921739c6d8dd6ab8
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-02 20:28:12 +0300

    add to class AboutCompanyPage method getTitleAboutCompanyText

--- src/main/java/by/itacademy/belaya/AboutCompanyPage.java
+++ src/main/java/by/itacademy/belaya/AboutCompanyPage.java
@@ -1,7 +1,15 @@
 package by.itacademy.belaya;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AboutCompanyPage {
     private WebDriver driver;
+
+    public String getTitleAboutCompanyText() {
+        By byXPath = By.xpath(AboutCompanyPageLocators.TITLE_ABOUT_COMPANY);
+        WebElement webElement = driver.findElement(byXPath);
+        return webElement.getText();
+    }
 }
