8b4be593713930183232c812d767e3cbc09db57b
Nikolai <n91056379@gmail.com>
2025-02-02 19:20:59 +0300

    add DynamicLoadingTest2 in c неявным ожиданием

--- src/test/java/by/itacademy/DynamicLoadingTest2.java
+++ src/test/java/by/itacademy/DynamicLoadingTest2.java
@@ -2,7 +2,11 @@ package by.itacademy;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -10,27 +14,27 @@ public class DynamicLoadingTest2 {
     private WebDriver driver;
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
     }
 
     @Test
     @DisplayName("Click start get text Hello World2")
-    public void testStart(){
+    public void testStart() {
         DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
         dynamicControlsPage.clickStart();
-        Waits.waitFor(4);
         DynamicLoadingPage dynamicLoadingPage = new DynamicLoadingPage(driver);
-        Waits.waitFor(4);
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement text = wait.until(ExpectedConditions.visibilityOfElementLocated(DynamicLoadingLocators.HELLO_WORLD1));
+        String actual = text.getText();
         Assertions.assertEquals("Hello World!", dynamicLoadingPage.getTextHelloWorld2());
     }
 
 
     @AfterEach
-    public void afterEach(){
+    public void afterEach() {
         driver.quit();
     }
 }
