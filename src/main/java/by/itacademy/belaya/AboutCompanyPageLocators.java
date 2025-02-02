f9fc94eab3ad34375c8880cebb7181c13e4ac912
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-02 21:40:37 +0300

    refactor class AboutCompanyPageLocators: change xPath

--- src/main/java/by/itacademy/belaya/AboutCompanyPageLocators.java
+++ src/main/java/by/itacademy/belaya/AboutCompanyPageLocators.java
@@ -1,5 +1,5 @@
 package by.itacademy.belaya;
 
 public class AboutCompanyPageLocators {
-    public static final String TITLE_ABOUT_COMPANY = "//a[text()='О компании']";
+    public static final String TITLE_ABOUT_COMPANY = "//h1[text()='Информация о компании']";
 }
