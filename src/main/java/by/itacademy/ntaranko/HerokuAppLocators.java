6125d5cd3c2b9ee0cb9f812de9f55da2e1169b0e
ntaranko <ntaranko@gmail.com>
2025-02-03 00:24:11 +0300

    refactor locators

--- src/main/java/by/itacademy/ntaranko/HerokuAppLocators.java
+++ src/main/java/by/itacademy/ntaranko/HerokuAppLocators.java
@@ -3,8 +3,7 @@ package by.itacademy.ntaranko;
 import org.openqa.selenium.By;
 
 public class HerokuAppLocators {
-    public static final By CHECKBOX_A = By.cssSelector("div#checkbox");
-    public static final By BUTTON_REMOVE = By.cssSelector("[onclick=\"swapCheckbox()\"]");
-    public static final By MESSAGE_It_S_GONE = By.cssSelector("p#message");
+    public static final By CHECKBOX_A = By.cssSelector("input[type=\"checkbox\"]");
+    public static final By BUTTON_REMOVE_ADD = By.cssSelector("[onclick=\"swapCheckbox()\"]");
+    public static final By MESSAGE_It_S_GONE_BACK = By.cssSelector("p#message");
 }
-
