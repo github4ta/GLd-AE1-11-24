bdb4a2580d6d328ced410b232f113617e1177a3a
ntaranko <ntaranko@gmail.com>
2025-02-02 21:09:39 +0300

    add locators LINK_TEXT_HEADPHONE and LINK_PARTIAL_TEXT_HEADPHONE

--- src/main/java/by/itacademy/ntaranko/HomePageLocators.java
+++ src/main/java/by/itacademy/ntaranko/HomePageLocators.java
@@ -6,4 +6,6 @@ public class HomePageLocators {
     public static final String FOOTER_COPYRIGHT = "//div[@class=\"footer-style__copy\"]";
     public static final String FOOTER_LINK_ABOUT_COMPANY = "//a[@href=\"https://blog.onliner.by/about\"]";
     public static final String BUTTON_CART = "//a[@title=\"Корзина\"]";
+    public static final String LINK_TEXT_HEADPHONE = "Наушники и гарнитуры";
+    public static final String LINK_PARTIAL_TEXT_HEADPHONE = "Наушники";
 }
