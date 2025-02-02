e473e10a31613f367c8f37509071100eb2254588
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-02 20:42:14 +0300

    add constructor to class ForumPage

--- src/main/java/by/itacademy/belaya/ForumPage.java
+++ src/main/java/by/itacademy/belaya/ForumPage.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.WebElement;
 public class ForumPage {
     private WebDriver driver;
 
+    public ForumPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
     public String getTitleForumText() {
         By byXPath = By.xpath(ForumPageLocators.TITLE_FORUM);
         WebElement webElement = driver.findElement(byXPath);
