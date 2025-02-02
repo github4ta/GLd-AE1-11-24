16799532866057aac38c14d026b504eb6592fe91
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-02 20:20:59 +0300

    add to class ForumPage method getTitleForumText

--- src/main/java/by/itacademy/belaya/ForumPage.java
+++ src/main/java/by/itacademy/belaya/ForumPage.java
@@ -1,7 +1,15 @@
 package by.itacademy.belaya;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ForumPage {
     private WebDriver driver;
+
+    public String getTitleForumText() {
+        By byXPath = By.xpath(ForumPageLocators.TITLE_FORUM);
+        WebElement webElement = driver.findElement(byXPath);
+        return webElement.getText();
+    }
 }
