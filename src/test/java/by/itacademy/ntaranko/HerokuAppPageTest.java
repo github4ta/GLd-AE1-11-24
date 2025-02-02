317688b7db8f5ab7b607037a60fdc39a78e6b383
ntaranko <ntaranko@gmail.com>
2025-02-03 00:57:33 +0300

    refactor - add tests

--- src/test/java/by/itacademy/ntaranko/HerokuAppPageTest.java
+++ src/test/java/by/itacademy/ntaranko/HerokuAppPageTest.java
@@ -19,7 +19,7 @@ public class HerokuAppPageTest {
     public void test1() {
         HerokuAppPage hp = new HerokuAppPage(driver);
         hp.clickRemoveAddButton();
-        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE_It_S_GONE_BACK);
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
 
         Assertions.assertFalse(hp.isElementPresent(HerokuAppLocators.CHECKBOX_A));
     }
@@ -29,12 +29,35 @@ public class HerokuAppPageTest {
     public void test2() {
         HerokuAppPage hp = new HerokuAppPage(driver);
         hp.clickRemoveAddButton();
-        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE_It_S_GONE_BACK);
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
 
         hp.clickRemoveAddButton();
-        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE_It_S_GONE_BACK);
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
 
         Assertions.assertTrue(hp.isElementPresent(HerokuAppLocators.CHECKBOX_A));
+    }
+
+    @Test
+    @DisplayName("Input field can be enabled")
+    public void test3() {
+        HerokuAppPage hp = new HerokuAppPage(driver);
+        hp.clickEnableDisableButton();
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
+
+        Assertions.assertEquals("false", hp.getPropertyOfElement(HerokuAppLocators.FIELD_INPUT_TEXT, "disabled"));
+    }
+
+    @Test
+    @DisplayName("Input field can be disabled")
+    public void test4() {
+        HerokuAppPage hp = new HerokuAppPage(driver);
+        hp.clickEnableDisableButton();
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
+
+        hp.clickEnableDisableButton();
+        hp.waitForVisibilityOfElement(HerokuAppLocators.MESSAGE);
+
+        Assertions.assertEquals("true", hp.getPropertyOfElement(HerokuAppLocators.FIELD_INPUT_TEXT, "disabled"));
     }
 
     @AfterEach
