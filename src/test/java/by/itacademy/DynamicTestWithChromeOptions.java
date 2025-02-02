4e988e2b8e3f24ab52d6a7f0f9db98ebeb3a5c32
Nikolai <n91056379@gmail.com>
2025-02-02 19:22:47 +0300

    add DynamicLoadingTest in c неявным ожиданием

---
+++ src/test/java/by/itacademy/DynamicTestWithChromeOptions.java
@@ -0,0 +1,65 @@
+package by.itacademy;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class DynamicTestWithChromeOptions {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        Duration duration = Duration.ofSeconds(5);
+        chromeOptions.setImplicitWaitTimeout(duration);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+    }
+
+    @Test
+    @DisplayName("Click remove")
+    public void testClick() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickRemove();
+        Assertions.assertEquals("It's gone!", dynamicControlsPage.getMessage());
+    }
+
+    @Test
+    @DisplayName("Click add get text It's back!")
+    public void testClick1() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickRemove();
+        dynamicControlsPage.clickAdd();
+        Assertions.assertEquals("It's back!", dynamicControlsPage.getItsBack());
+
+    }
+
+    @Test
+    @DisplayName("Click button enable get text It's enable ")
+    public void testClick2() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickEnable();
+        Assertions.assertEquals("It's enabled!", dynamicControlsPage.getItsEnable());
+
+    }
+
+    @Test
+    @DisplayName("Click button disable and get text It's disabled")
+    public void testClick3() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickEnable();
+        dynamicControlsPage.clickDisable();
+        Assertions.assertEquals("It's disabled!", dynamicControlsPage.getItsDisable());
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+}
