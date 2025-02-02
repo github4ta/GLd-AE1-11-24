cb3fb3bd3d89a1a2da8b5fd51b16e975533af4d8
ntaranko <ntaranko@gmail.com>
2025-02-03 00:24:56 +0300

    refactor - add methods

--- src/main/java/by/itacademy/ntaranko/HerokuAppPage.java
+++ src/main/java/by/itacademy/ntaranko/HerokuAppPage.java
@@ -3,6 +3,12 @@ package by.itacademy.ntaranko;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HerokuAppPage {
     private WebDriver driver;
@@ -11,8 +17,13 @@ public class HerokuAppPage {
         this.driver = driver;
     }
 
-    public void clickRemoveButton() {
-        driver.findElement(HerokuAppLocators.BUTTON_REMOVE).click();
+    public void clickRemoveAddButton() {
+        driver.findElement(HerokuAppLocators.BUTTON_REMOVE_ADD).click();
+    }
+
+    public void waitForVisibilityOfElement(By locator) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public boolean isElementPresent(By locator) {
