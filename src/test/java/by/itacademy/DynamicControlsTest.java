0d922e16a703dc3daf02450fd72fd2d96e612e08
Nikolai <n91056379@gmail.com>
2025-02-03 12:55:14 +0300

    add method

--- src/test/java/by/itacademy/DynamicControlsTest.java
+++ src/test/java/by/itacademy/DynamicControlsTest.java
@@ -3,13 +3,19 @@ package by.itacademy;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class DynamicControlsTest {
     private WebDriver driver;
 
     @BeforeEach
     public void beforeEach() {
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        Duration duration = Duration.ofSeconds(5);
+        chromeOptions.setImplicitWaitTimeout(duration)
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/dynamic_controls");
     }
@@ -19,8 +25,7 @@ public class DynamicControlsTest {
     public void testClick() {
         DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
         dynamicControlsPage.clickRemove();
-
-        Assertions.assertEquals("It's gone!",dynamicControlsPage.getMessage());
+        Assertions.assertEquals("It's gone!", dynamicControlsPage.getMessage());
     }
 
     @Test
@@ -28,10 +33,8 @@ public class DynamicControlsTest {
     public void testClick1() {
         DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
         dynamicControlsPage.clickRemove();
-
         dynamicControlsPage.clickAdd();
-
-        Assertions.assertEquals("It's back!",dynamicControlsPage.getItsBack());
+        Assertions.assertEquals("It's back!", dynamicControlsPage.getItsBack());
 
     }
 
@@ -40,8 +43,7 @@ public class DynamicControlsTest {
     public void testClick2() {
         DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
         dynamicControlsPage.clickEnable();
-
-        Assertions.assertEquals("It's enabled!",dynamicControlsPage.getItsEnable());
+        Assertions.assertEquals("It's enabled!", dynamicControlsPage.getItsEnable());
 
     }
 
@@ -50,11 +52,27 @@ public class DynamicControlsTest {
     public void testClick3() {
         DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
         dynamicControlsPage.clickEnable();
-
         dynamicControlsPage.clickDisable();
+        Assertions.assertEquals("It's disabled!", dynamicControlsPage.getItsDisable());
 
-        Assertions.assertEquals("It's disabled!",dynamicControlsPage.getItsDisable());
+    }
 
+    @Test
+    public void testText() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickRemove();
+        Waits.waitFor(4);
+        Assertions.assertTrue(dynamicControlsPage.isCheckbox(DynamicControlsLocators.CHECKBOX_A));
+    }
+
+    @Test
+    @DisplayName("")
+    public void testText1() {
+        DynamicControlsPage dynamicControlsPage = new DynamicControlsPage(driver);
+        dynamicControlsPage.clickRemove();
+        dynamicControlsPage.clickAdd();
+        Waits.waitFor(4);
+        Assertions.assertFalse(dynamicControlsPage.isCheckbox(DynamicControlsLocators.CHECKBOX_A));
     }
 
 
