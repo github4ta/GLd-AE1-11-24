455d2080f9fe095f57ad23a5b02a63c34273b50d
Sergei Tsarik <serge.tsarik@gmail.com>
2025-02-03 22:13:31 +0300

    add step

--- src/test/java/by/onliner/LoginTest.java
+++ src/test/java/by/onliner/LoginTest.java
@@ -1,38 +1,23 @@
 package by.onliner;
 
-import by.onliner.pages.CookiePage;
-import by.onliner.pages.HomePage;
 import by.onliner.pages.LoginPage;
 import by.onliner.singleton.Singleton;
+import by.onliner.steps.HomeStep;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
 
 public class LoginTest {
 
     @BeforeEach
     public void beforeEach() {
-        HomePage homePage = new HomePage();
-        homePage.open();
-
-        CookiePage cookiePage = new CookiePage();
-        cookiePage.clickCookie();
-
-        homePage = new HomePage();
-        homePage.clickButtonLogin();
+        new HomeStep().openLoginPage();
     }
 
     @Test
     public void testLoginIsOpened() {
-        LoginPage loginPage = new LoginPage();
-        String actual = loginPage.getTitleText();
-
-        Assertions.assertEquals("Вход", actual);
+        Assertions.assertEquals("Вход", new LoginPage().getTitleText());
     }
 
     @AfterEach
