1c6459afa325eef32ca1f6ab03ffa2913eb3a3cc
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-02-03 20:28:15 +0300

    refactor code HomeTest.java

--- src/test/java/by/onliner/HomeTest.java
+++ src/test/java/by/onliner/HomeTest.java
@@ -2,25 +2,36 @@ package by.onliner;
 
 import by.onliner.pages.CookiePage;
 import by.onliner.pages.HomePage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
 
 public class HomeTest {
+    private WebDriver driver;
 
-    @Test
-    public void testPageOpened(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
-        driver.manage().window().maximize();
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.get("https://money.onliner.by/");
+        driver.manage().window().maximize();
         CookiePage cookiePage = new CookiePage(driver);
         cookiePage.clickCookie();
+    }
+
+    @Test
+    public void testPageOpened() {
         HomePage homePage = new HomePage(driver);
         String actual = homePage.getTextCopyright();
         Assertions.assertEquals("© 2001—2025 Onlíner", actual);
     }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
