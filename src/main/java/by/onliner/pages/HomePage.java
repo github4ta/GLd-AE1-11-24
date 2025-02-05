f7f73e8fcc813b5b05d2a7c7c767bdc337066b11
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-05 23:43:34 +0300

    add fields COPYRIGHT_TEXT_LOCATOR ENTER_TEXT_LOCATOR SEARCH_TEXT_LOCATOR NEWS_LINK_LOCATOR AVTOBARAHOLKA_LINK_LOCATOR HOUSES_AND_FLATS_LINK_LOCATOR SERVICES_LINK_LOCATOR BARAHOLKA_LINK_LOCATOR FORUM_LINK_LOCATOR VACANCIES_LINK_LOCATOR ADVERTISING_LINK_LOCATOR PLANS_LINK_LOCATOR

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -1,6 +1,7 @@
 package by.onliner.pages;
 
 import by.onliner.singleton.Singleton;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -10,4 +11,26 @@ public class HomePage {
     public HomePage() {
         this.driver = Singleton.getDriver();
     }
+
+    private final By COPYRIGHT_TEXT_LOCATOR = By.xpath("//div[@class='footer-style__copy']");
+    private final By ENTER_TEXT_LOCATOR = By.xpath("//div[@class='auth-bar__item auth-bar__item--text']");
+    private final By SEARCH_TEXT_LOCATOR = By.xpath("//input[@name='query']");
+    private final By NEWS_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Новости']");
+    private final By AVTOBARAHOLKA_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Автобарахолка']");
+    private final By HOUSES_AND_FLATS_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Дома и квартиры']");
+    private final By SERVICES_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Услуги']");
+    private final By BARAHOLKA_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Барахолка']");
+    private final By FORUM_LINK_LOCATOR =
+            By.xpath("//span[@class='b-main-navigation__text' and text()='Форум']");
+    private final By VACANCIES_LINK_LOCATOR =
+            By.xpath("//a[@href='https://blog.onliner.by/vacancy']");
+    private final By ADVERTISING_LINK_LOCATOR =
+            By.xpath("//a[@href='https://b2breg.onliner.by/advertising']");
+    private final By PLANS_LINK_LOCATOR =
+            By.xpath("//a[@href='https://docs.google.com/spreadsheets/d/1SGFaTkV_Ru4vI29ml9yvR-dMz9rOl7DVVpKk64w5lqM/preview']");
 }
