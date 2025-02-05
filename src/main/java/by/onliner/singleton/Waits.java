31f40f538f1839b5d36675d5d6a12253b96da571
Sergei Tsarik <serge.tsarik@gmail.com>
2025-02-05 19:28:11 +0300

    add waits

---
+++ src/main/java/by/onliner/singleton/Waits.java
@@ -0,0 +1,12 @@
+package by.onliner.singleton;
+
+public class Waits {
+
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
