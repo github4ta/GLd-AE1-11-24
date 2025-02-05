488e84b572e87d15924b03cb57e3b823e903c21d
Nikolai <n91056379@gmail.com>
2025-02-05 23:26:29 +0300

    refactor ForumTest

--- src/test/java/by/onliner/ForumTest.java
+++ src/test/java/by/onliner/ForumTest.java
@@ -1,30 +1,19 @@
 package by.onliner;
 
 import by.onliner.pages.ForumPage;
+import by.onliner.singleton.Singleton;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
 
 public class ForumTest {
-    private WebDriver driver;
-
-    @BeforeEach
-    public void beforeEach() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://www.onliner.by/");
-
-    }
 
     @Test
     public void test() {
-        ForumPage forumPage = new ForumPage(driver);
+
+        ForumPage forumPage = new ForumPage();
+        forumPage.open();
+
         forumPage.clickForum();
         String actual = forumPage.getTextForum();
         Assertions.assertEquals("Форум", actual);
@@ -33,6 +22,6 @@ public class ForumTest {
 
     @AfterEach
     public void afterEach() {
-        driver.quit();
+        Singleton.quit();
     }
 }
