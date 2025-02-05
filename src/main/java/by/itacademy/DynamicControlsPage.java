f27d918662c0b5e41d0a67db1ac2ebf47ef1e84e
Sergei Tsarik <serge.tsarik@gmail.com>
2025-02-05 21:27:37 +0300

    clean up repo

--- src/main/java/by/itacademy/DynamicControlsPage.java
+++
@@ -1,6 +0,0 @@
-package by.itacademy;
-
-public class DynamicControlsPage {
-
-
-}
--- src/main/java/by/onliner/HelloBelaya.java
+++
@@ -1,7 +0,0 @@
-package by.onliner;
-
-public class HelloBelaya {
-    public static void main(String[] args) {
-        System.out.println("Hello from Belaya");
-    }
-}
