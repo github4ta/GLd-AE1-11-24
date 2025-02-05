ec633d1cca5981ff10c92f6c4a52e680415df8c2
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-05 23:39:23 +0300

    add WebDriver and constructor

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -1,4 +1,13 @@
 package by.onliner.pages;
 
+import by.onliner.singleton.Singleton;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage() {
+        this.driver = Singleton.getDriver();
+    }
 }
