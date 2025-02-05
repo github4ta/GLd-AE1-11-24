b58612ab52b453ba5d9d6e3414f0a6f536179b9d
Sergei Tsarik <serge.tsarik@gmail.com>
2025-02-05 21:30:37 +0300

    clean up repo

--- src/main/java/by/onliner/HelloPsekha.java
+++
@@ -1,7 +0,0 @@
-package by.onliner;
-
-public class HelloPsekha {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/main/java/by/onliner/HelloTsarik.java
+++
@@ -1,7 +0,0 @@
-package by.onliner;
-
-public class HelloTsarik {
-    public static void main(String[] args) {
-        System.out.println("Hello from Tsarik");
-    }
-}
