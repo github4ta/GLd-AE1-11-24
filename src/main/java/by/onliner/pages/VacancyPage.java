4c7e7c73c0fcbfd5a7e630fabd2173701ed682d7
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-02-06 01:44:27 +0300

    add WebDriver and constructor

--- src/main/java/by/onliner/pages/VacancyPage.java
+++ src/main/java/by/onliner/pages/VacancyPage.java
@@ -1,4 +1,13 @@
 package by.onliner.pages;
 
+import by.onliner.singleton.Singleton;
+import org.openqa.selenium.WebDriver;
+
 public class VacancyPage {
+
+     private WebDriver driver;
+
+    public VacancyPage() {
+        this.driver = Singleton.getDriver();
+    }
 }
