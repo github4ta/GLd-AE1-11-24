1d8b2c67e0155e62f2363f4ffbd17ae3a56fd724
Nikolai <n91056379@gmail.com>
2025-02-05 22:04:19 +0300

    add ForumPage

---
+++ src/main/java/by/onliner/pages/ForumPage.java
@@ -0,0 +1,24 @@
+package by.onliner.pages;
+
+import by.onliner.singleton.Singleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ForumPage {
+    private final By CLICK_TITLE_FORUM = By.xpath("(//span[@class=\"b-main-navigation__text\"])[7]");
+    private final By TEXT_FORUM = By.xpath("//h1[@class='m-title']");
+
+    private WebDriver driver;
+
+    public ForumPage(WebDriver driver) {
+        this.driver = Singleton.getDriver();
+    }
+
+    public void clickForum() {
+        driver.findElement(CLICK_TITLE_FORUM).click();
+    }
+
+    public String getTextForum(){
+        return driver.findElement(TEXT_FORUM).getText();
+    }
+}
