1d2b1b9a1dd714147caa033e80edd624dce1f6d5
Артём <artem.kurepin999@gmail.com>
2025-02-08 17:52:39 +0300

    add locator

--- src/main/java/by/onliner/pages/RulesForReturning.java
+++ src/main/java/by/onliner/pages/RulesForReturning.java
@@ -1,10 +1,13 @@
 package by.onliner.pages;
 
 import by.onliner.singleton.Singleton;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RulesForReturning {
     private WebDriver driver;
+
+    private final By HEADER_RULES=By.xpath("//div[@class='news-header__title']//h1");
 
     public RulesForReturning(WebDriver driver) {
         this.driver = Singleton.getDriver();
