0422f731af19ebbc34d1b1cea52c7333d25d6dde
VladimirBordiyan <waldstall@gmail.com>
2025-02-09 01:10:38 +0300

    Revert "add BaraholkaPage"
    This reverts commit 1bed8261c5210f68c4bbf19ab9f09ec102b72bb9.

--- src/main/java/by/onliner/pages/PageObject.java
+++
@@ -1,7 +0,0 @@
-package by.onliner.pages;
-
-import org.openqa.selenium.By;
-
-public class PageObject {
-    private final By SUPPORT_REQUEST = By.xpath("//h1[text()='Запрос в службу поддержки']");
-}
