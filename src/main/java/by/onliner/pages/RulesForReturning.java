a9fa01302992b5a67ba0f6f0cc0c2d027e52ef94
Артём <artem.kurepin999@gmail.com>
2025-02-08 17:49:56 +0300

    add URL link and method open add method getHeaderText add locator add webdriver and constructor

--- src/main/java/by/onliner/pages/RulesForReturning.java
+++ src/main/java/by/onliner/pages/RulesForReturning.java
@@ -1,5 +1,25 @@
 package by.onliner.pages;
 
-public class RulesForReturning {
+import by.onliner.singleton.Singleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class RulesForReturning {
+    private WebDriver driver;
+
+    private final By HEADER_RULES = By.xpath("//div[@class='news-header__title']//h1");
+    private final String URL = "https://blog.onliner.by/pravila-vozvrata-tovarov-i-deneg";
+
+    public RulesForReturning(WebDriver driver) {
+        this.driver = Singleton.getDriver();
+    }
+
+    public String getHeaderText() {
+        return driver.findElement(HEADER_RULES).getText();
+    }
+
+    public RulesForReturning open() {
+        driver.get(URL);
+        return this;
+    }
 }
