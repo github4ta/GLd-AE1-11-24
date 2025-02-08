2a008cd5742d8f75b3a462df6dfe3485a5cde9b4
GutenMorgen <waldstall@gmail.com>
2025-02-09 01:12:16 +0300

    add PageObject

--- src/main/java/by/onliner/pages/PageObject.java
+++ src/main/java/by/onliner/pages/PageObject.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 
 public class PageObject {
     private final WebDriver driver;
-    private final By SUPPORT_REQUEST_PAGE = By.xpath( "//h1[text()='Запрос в службу поддержки']");
+    private final By SUPPORT_REQUEST_PAGE = By.xpath("//h1[text()='Запрос в службу поддержки']");
 
     public PageObject() {
         this.driver = Singleton.getDriver();
