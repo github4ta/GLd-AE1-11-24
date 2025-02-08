18b0dfbdbea5054478284cefdf11c992d6eeb7e1
GutenMorgen <waldstall@gmail.com>
2025-02-09 00:25:23 +0300

    add BaraholkaPage

--- src/main/java/by/onliner/pages/PageObject.java
+++ src/main/java/by/onliner/pages/PageObject.java
@@ -1,7 +1,14 @@
 package by.onliner.pages;
 
+import by.onliner.singleton.Singleton;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class PageObject {
-    private final By SUPPORT_REQUEST_PAGE = By.xpath("//h1[text()='Запрос в службу поддержки']");
+
+    private final By SUPPORT_REQUEST_PAGE = By.xpath("//a[@href='https://support.onliner.by']");
+
+
+
+    }
 }
