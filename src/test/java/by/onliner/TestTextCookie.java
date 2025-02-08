4af665ebab74e7d2fb5e7bf77513439acbabb94a
Alex Linnik <SanchezAnd3@gmail.com>
2025-02-08 15:55:06 +0300

    Добавил пробный метод и ест для работы с модальным окном Cookie

---
+++ src/test/java/by/onliner/TestTextCookie.java
@@ -0,0 +1,23 @@
+package by.onliner;
+
+import by.onliner.pages.SettingCookie;
+import by.onliner.singleton.Singleton;
+import org.junit.jupiter.api.*;
+
+public class TestTextCookie {
+    @BeforeEach
+    public void beforeEach() {
+        new SettingCookie().open();
+    }
+
+    @Test
+    @DisplayName("We check the presence of the title and the text in it.")
+    public void getTextCookieTitle() {
+        Assertions.assertEquals("Мы используем cookie, чтобы сделать сайт удобнее", new SettingCookie().getTextCookieTitle());
+    }
+
+    @AfterEach
+    public void afterEach(){
+        Singleton.quit();
+    }
+}
