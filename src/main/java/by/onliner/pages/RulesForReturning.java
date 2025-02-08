9d6f42d42bb3506da32499806a17bacd8a11018c
Артём <artem.kurepin999@gmail.com>
2025-02-08 17:56:24 +0300

    add URL link and method open

--- src/main/java/by/onliner/pages/RulesForReturning.java
+++ src/main/java/by/onliner/pages/RulesForReturning.java
@@ -8,12 +8,18 @@ public class RulesForReturning {
     private WebDriver driver;
 
     private final By HEADER_RULES = By.xpath("//div[@class='news-header__title']//h1");
+    private final String URL = "https://blog.onliner.by/pravila-vozvrata-tovarov-i-deneg";
 
     public RulesForReturning(WebDriver driver) {
         this.driver = Singleton.getDriver();
     }
 
-    public String getHeaderText(){
+    public String getHeaderText() {
         return driver.findElement(HEADER_RULES).getText();
+    }
+
+    public RulesForReturning open() {
+        driver.get(URL);
+        return this;
     }
 }
