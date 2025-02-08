10f032e2af5cdb263280fe7f46da4f64cf76c5d8
Alex Linnik <SanchezAnd3@gmail.com>
2025-02-08 15:55:06 +0300

    Добавил пробный метод и ест для работы с модальным окном Cookie

---
+++ src/main/java/by/onliner/pages/SettingCookie.java
@@ -0,0 +1,25 @@
+package by.onliner.pages;
+
+import by.onliner.singleton.Singleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SettingCookie {
+
+    public static String URL = "https://www.onliner.by/";
+    public static By TEXT_TITLE = By.xpath("//div[text()='Мы используем cookie, чтобы сделать сайт удобнее']");
+
+    private WebDriver driver;
+
+    public SettingCookie() {
+        this.driver = Singleton.getDriver();
+    }
+
+    public String getTextCookieTitle(){
+        return driver.findElement(TEXT_TITLE).getText();
+    }
+
+    public void open(){
+        driver.get(URL);
+    }
+}
