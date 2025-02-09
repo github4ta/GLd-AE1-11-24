b71a602ec9b5edc0fa8a54ccf6e125edad373108
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-02-10 00:54:12 +0300

    rename class MainPage into HomePage

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -4,16 +4,16 @@ import by.onliner.singleton.Singleton;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class MainPage {
+public class HomePage {
     private WebDriver driver;
     private final String URL = "https://www.onliner.by/";
     private final By SERVICE_SECTION_BUTTON = By.xpath("//a [@href='https://s.onliner.by/tasks' and @class='b-main-navigation__link']");
 
-    public MainPage(WebDriver driver) {
+    public HomePage(WebDriver driver) {
         this.driver = Singleton.getDriver();
     }
 
-    public MainPage openDriver() {
+    public HomePage openPage() {
         driver.get(URL);
         return this;
     }
