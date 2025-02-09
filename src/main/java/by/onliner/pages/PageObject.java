f5ea6924d2fe6fa3c1ca3f7a32b30405c76014ca
GutenMorgen <waldstall@gmail.com>
2025-02-09 10:05:04 +0300

    add PageObject

--- src/main/java/by/onliner/pages/PageObject.java
+++ src/main/java/by/onliner/pages/PageObject.java
@@ -3,9 +3,6 @@ package by.onliner.pages;
 import by.onliner.singleton.Singleton;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
