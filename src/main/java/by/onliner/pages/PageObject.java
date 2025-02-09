b804ea9f699dcb03af31efc1331a2498efeb18e2
GutenMorgen <waldstall@gmail.com>
2025-02-09 11:24:50 +0300

    add PageObject

--- src/main/java/by/onliner/pages/PageObject.java
+++ src/main/java/by/onliner/pages/PageObject.java
@@ -13,7 +13,6 @@ public class PageObject {
     private final WebDriver driver;
     private final By HEADER_PAGE = By.xpath("//h1[text()='Публичные договоры']");
 
-
     public PageObject() {
         this.driver = Singleton.getDriver();
     }
