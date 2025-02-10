79f2b65a33cc7ee98e9d4219fea512447aa48f11
vicusa01 <101738686+vicusa01@users.noreply.github.com>
2025-02-10 18:14:45 +0300

    UI005 - Услуги (#62)
    * add MainPage
    
    * add ServicePage
    
    * add ServicePageTest
    
    * refactor ServicesPageTest.java
    
    * rename class MainPage into HomePage
    
    * refactor ServicePage
    
    * Delete src/test/java/by/onliner/ServicesPageTest.java
    
    ---------
    
    Co-authored-by: viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -6,6 +6,17 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver;
+    private final String URL = "https://www.onliner.by/";
+    private final By SERVICE_SECTION_BUTTON = By.xpath("//a [@href='https://s.onliner.by/tasks' and @class='b-main-navigation__link']");
+
+    public HomePage openPage() {
+        driver.get(URL);
+        return this;
+    }
+
+    public ServicesPage clickServiceSectionButton() {
+        driver.findElement(SERVICE_SECTION_BUTTON).click();
+        return new ServicesPage(driver);
 
     public HomePage() {
         this.driver = Singleton.getDriver();
